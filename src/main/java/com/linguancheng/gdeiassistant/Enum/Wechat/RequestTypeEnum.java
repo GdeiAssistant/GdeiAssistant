36d6cc15bc95b0f4778892f51887fea83074f1ba
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-10-05 04:39:12 +0800

    添加账号绑定和校园卡的枚举

--- src/main/java/com/linguancheng/gdeiassistant/Enum/Wechat/RequestTypeEnum.java
+++ src/main/java/com/linguancheng/gdeiassistant/Enum/Wechat/RequestTypeEnum.java
@@ -2,9 +2,13 @@ package com.linguancheng.gdeiassistant.Enum.Wechat;
 
 public enum RequestTypeEnum {
 
-    TODAY_SCHEDULE("课表1"),
+    TODAY_SCHEDULE("课表"),
 
-    GRADE("成绩");
+    GRADE("成绩"),
+
+    CARD("校园卡"),
+
+    ATTACH("绑定账号");
 
     RequestTypeEnum(String type) {
         this.type = type;
@@ -16,9 +20,9 @@ public enum RequestTypeEnum {
         return type;
     }
 
-    public static RequestTypeEnum getRequestTypeEnumByFunctionName(String functionName){
-        for(RequestTypeEnum requestTypeEnum:RequestTypeEnum.values()){
-            if(requestTypeEnum.getType().equalsIgnoreCase(functionName)){
+    public static RequestTypeEnum getRequestTypeEnumByFunctionName(String functionName) {
+        for (RequestTypeEnum requestTypeEnum : RequestTypeEnum.values()) {
+            if (requestTypeEnum.getType().equalsIgnoreCase(functionName)) {
                 return requestTypeEnum;
             }
         }

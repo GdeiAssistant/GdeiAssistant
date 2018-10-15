18c3ead14e0126562ba568e6ce4563a41f6c82aa
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-10-16 02:38:06 +0800

    教务系统会话同步时间戳校验失败异常

---
+++ src/main/java/com/linguancheng/gdeiassistant/Exception/QueryException/TimeStampIncorrectException.java
@@ -0,0 +1,15 @@
+package com.linguancheng.gdeiassistant.Exception.QueryException;
+
+/**
+ * 快速连接教务系统时，若教务系统进行时间戳验证失败，则抛出该异常
+ */
+public class TimeStampIncorrectException extends Exception {
+
+    public TimeStampIncorrectException() {
+        super();
+    }
+
+    public TimeStampIncorrectException(String message) {
+        super(message);
+    }
+}

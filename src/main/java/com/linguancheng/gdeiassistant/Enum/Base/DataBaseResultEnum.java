47adda018ca4118ce9aca607f55c667ce49ae99f
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-10-17 17:56:03 +0800

    添加查询条件错误的枚举选项

--- src/main/java/com/linguancheng/gdeiassistant/Enum/Base/DataBaseResultEnum.java
+++ src/main/java/com/linguancheng/gdeiassistant/Enum/Base/DataBaseResultEnum.java
@@ -1,5 +1,5 @@
 package com.linguancheng.gdeiassistant.Enum.Base;
 
 public enum DataBaseResultEnum {
-    SUCCESS, ERROR, INCORRECT_USERNAME, EMPTY_RESULT
+    SUCCESS, ERROR, ERROR_CONDITION, INCORRECT_USERNAME, EMPTY_RESULT
 }

b68af1ccdcff813842d4eb93a947aa50c5c06641
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2018-10-21 22:56:14 +0800

    用户登录凭证

--- src/main/java/com/linguancheng/gdeiassistant/Pojo/UserLogin/UserCertificate.java
+++ src/main/java/com/linguancheng/gdeiassistant/Pojo/UserLogin/UserCertificate.java
@@ -1,6 +1,5 @@
 package com.gdeiassistant.gdeiassistant.Pojo.UserLogin;
 
-import com.gdeiassistant.gdeiassistant.Enum.Base.LoginResultEnum;
 import com.gdeiassistant.gdeiassistant.Pojo.Entity.User;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -9,26 +8,16 @@ import org.springframework.stereotype.Component;
  * 广东二师助手团队 林冠成 版权所有
  * All rights reserved © 2016 - 2018
  * Author:林冠成
- * Date:2018/10/15
+ * Date:2018/10/21
  */
 
 @Component
 @Scope("prototype")
-public class UserLoginResult {
-
-    private LoginResultEnum loginResultEnum;
+public class UserCertificate {
 
     private User user;
 
     private Long timestamp;
-
-    public LoginResultEnum getLoginResultEnum() {
-        return loginResultEnum;
-    }
-
-    public void setLoginResultEnum(LoginResultEnum loginResultEnum) {
-        this.loginResultEnum = loginResultEnum;
-    }
 
     public User getUser() {
         return user;

359935fe1165ab2effd3bbfc30992c0aaae8eadf
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2018-11-11 22:02:13 +0800

    使用JWT用于Rest接口身份认证

---
+++ src/main/java/com/linguancheng/gdeiassistant/Repository/Redis/Token/LoginTokenDao.java
@@ -0,0 +1,21 @@
+package com.gdeiassistant.gdeiassistant.Repository.Redis.Token;
+
+import com.gdeiassistant.gdeiassistant.Pojo.Entity.AccessToken;
+import com.gdeiassistant.gdeiassistant.Pojo.Entity.RefreshToken;
+
+/**
+ * 广东二师助手团队 林冠成 版权所有
+ * All rights reserved © 2016 - 2018
+ * Author:林冠成
+ * Date:2018/10/28
+ */
+public interface LoginTokenDao {
+
+    public String QueryToken(String signature);
+
+    public Boolean SaveAccessToken(AccessToken token);
+
+    public Boolean SaveRefreshToken(RefreshToken token);
+
+    public Boolean DeleteToken(String signature);
+}

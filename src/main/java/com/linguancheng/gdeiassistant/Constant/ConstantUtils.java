d31198dfb2c8d28e300764a509cdb9f0bcaa0ff7
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-11-11 22:04:13 +0800

    请求接口错误码

---
+++ src/main/java/com/linguancheng/gdeiassistant/Constant/ConstantUtils.java
@@ -0,0 +1,27 @@
+package com.linguancheng.gdeiassistant.Constant;
+
+/**
+ * 广东二师助手团队 林冠成 版权所有
+ * All rights reserved © 2016 - 2018
+ * Author:林冠成
+ * Date:2018/11/5
+ */
+public class ConstantUtils {
+
+    /**
+     * 令牌校验错误码
+     */
+
+    //权限令牌过期
+    public static final int TOKEN_EXPIRED_EXCEPTION = 401;
+
+    //异常登录地
+    public static final int UNUSUAL_LOCATION_EXCEPTION = 402;
+
+    //令牌校验失败
+    public static final int TOKEN_NOT_MATCHING = 403;
+
+    //令牌校验服务异常
+    public static final int TOKEN_SERVER_ERROR = 404;
+
+}

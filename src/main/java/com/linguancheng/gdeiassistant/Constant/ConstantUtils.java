a71e0e1e9d1dd1cb314e63aa4bb90e1cadfe3517
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-11-13 00:54:37 +0800

    更新错误码常量表

--- src/main/java/com/linguancheng/gdeiassistant/Constant/ConstantUtils.java
+++ src/main/java/com/linguancheng/gdeiassistant/Constant/ConstantUtils.java
@@ -8,20 +8,19 @@ package com.linguancheng.gdeiassistant.Constant;
  */
 public class ConstantUtils {
 
-    /**
-     * 令牌校验错误码
-     */
+    //请求参数不合法
+    public static final int INCORRECT_REQUEST_PARAM = 40001;
 
     //权限令牌过期
-    public static final int TOKEN_EXPIRED_EXCEPTION = 401;
+    public static final int TOKEN_EXPIRED_EXCEPTION = 40101;
 
     //异常登录地
-    public static final int UNUSUAL_LOCATION_EXCEPTION = 402;
+    public static final int UNUSUAL_LOCATION_EXCEPTION = 40102;
 
     //令牌校验失败
-    public static final int TOKEN_NOT_MATCHING = 403;
+    public static final int TOKEN_NOT_MATCHING = 40103;
 
     //令牌校验服务异常
-    public static final int TOKEN_SERVER_ERROR = 404;
+    public static final int TOKEN_SERVER_ERROR = 40104;
 
 }

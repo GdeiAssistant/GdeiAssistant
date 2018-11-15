dd4427e86ddc527735f41dcd7485d45e743f6b23
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-11-15 14:08:39 +0800

    二手交易和失物招领状态异常类

---
+++ src/main/java/com/linguancheng/gdeiassistant/Exception/DatabaseException/ConfirmedStateException.java
@@ -0,0 +1,11 @@
+package com.linguancheng.gdeiassistant.Exception.DatabaseException;
+
+public class ConfirmedStateException extends Exception {
+
+    public ConfirmedStateException() {
+    }
+
+    public ConfirmedStateException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/linguancheng/gdeiassistant/Exception/DatabaseException/NotAvailableStateException.java
@@ -0,0 +1,11 @@
+package com.linguancheng.gdeiassistant.Exception.DatabaseException;
+
+public class NotAvailableStateException extends Exception {
+
+    public NotAvailableStateException() {
+    }
+
+    public NotAvailableStateException(String message) {
+        super(message);
+    }
+}

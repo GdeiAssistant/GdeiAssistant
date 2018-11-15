f70dbcd20bb9c4abd1ee8aa8aee2253295ec75e2
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2018-11-15 14:08:39 +0800

    二手交易和失物招领状态异常类

---
+++ src/main/java/com/linguancheng/gdeiassistant/Exception/DatabaseException/NotAvailableStateException.java
@@ -0,0 +1,11 @@
+package com.gdeiassistant.gdeiassistant.Exception.DatabaseException;
+
+public class NotAvailableStateException extends Exception {
+
+    public NotAvailableStateException() {
+    }
+
+    public NotAvailableStateException(String message) {
+        super(message);
+    }
+}

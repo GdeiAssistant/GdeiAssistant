9ea22cb0ac2040cc1a89b04c83622b0e2a806138
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-11-18 19:46:09 +0800

    修改微信小程序通过Code获取微信用户ID的方法逻辑

--- src/main/java/com/linguancheng/gdeiassistant/Service/Wechat/WechatAppService.java
+++ src/main/java/com/linguancheng/gdeiassistant/Service/Wechat/WechatAppService.java
@@ -1,11 +1,14 @@
 package com.linguancheng.gdeiassistant.Service.Wechat;
 
-import com.linguancheng.gdeiassistant.Enum.Base.BoolResultEnum;
+import com.linguancheng.gdeiassistant.Exception.CommonException.ServerErrorException;
 import com.linguancheng.gdeiassistant.Pojo.Entity.WechatUser;
-import com.linguancheng.gdeiassistant.Pojo.Result.BaseResult;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -35,22 +38,20 @@ public class WechatAppService {
      * @param js_code
      * @return
      */
-    public BaseResult<WechatUser, BoolResultEnum> GetWechatUser(String js_code) {
-        BaseResult<WechatUser, BoolResultEnum> result = new BaseResult<>();
-        JSONObject jsonObject = restTemplate.getForObject("https://api.weixin.qq.com/sns/jscode2session?appid="
-                + appId + "&secret=" + appSecret + "&js_code="
-                + js_code + "&grant_type=authorization_code", JSONObject.class);
+    public WechatUser GetWechatUser(String js_code) throws Exception {
+        ResponseEntity<String> responseEntity = restTemplate.exchange("https://api.weixin.qq.com/sns/jscode2session?appid="
+                        + appId + "&secret=" + appSecret + "&js_code="
+                        + js_code + "&grant_type=authorization_code", HttpMethod.GET
+                , new HttpEntity<>(new HttpHeaders()), String.class);
+        JSONObject jsonObject = JSONObject.fromObject(responseEntity.getBody());
         if (jsonObject.has("openid")) {
             WechatUser wechatUser = new WechatUser();
             wechatUser.setOpenid(jsonObject.getString("openid"));
             if (jsonObject.has("unionid")) {
                 wechatUser.setUnionid(jsonObject.getString("unionid"));
             }
-            result.setResultData(wechatUser);
-            result.setResultType(BoolResultEnum.SUCCESS);
-        } else {
-            result.setResultType(BoolResultEnum.ERROR);
+            return wechatUser;
         }
-        return result;
+        throw new ServerErrorException("获取微信ID异常");
     }
 }

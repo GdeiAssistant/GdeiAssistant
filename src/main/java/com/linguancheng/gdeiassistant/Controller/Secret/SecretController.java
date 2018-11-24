77faf3ff730c3619fe41941cb315b65fba7274f8
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-11-25 01:46:28 +0800

    分离普通控制器和Rest控制器

--- src/main/java/com/linguancheng/gdeiassistant/Controller/Secret/SecretController.java
+++ src/main/java/com/linguancheng/gdeiassistant/Controller/Secret/SecretController.java
@@ -1,19 +1,14 @@
 package com.linguancheng.gdeiassistant.Controller.Secret;
 
-import com.linguancheng.gdeiassistant.Enum.Base.DataBaseResultEnum;
+import com.linguancheng.gdeiassistant.Exception.DatabaseException.DataNotExistException;
 import com.linguancheng.gdeiassistant.Pojo.Entity.Secret;
-import com.linguancheng.gdeiassistant.Pojo.Result.DataJsonResult;
-import com.linguancheng.gdeiassistant.Pojo.Result.JsonResult;
-import com.linguancheng.gdeiassistant.Pojo.Result.BaseResult;
 import com.linguancheng.gdeiassistant.Service.Secret.SecretService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,44 +20,34 @@ public class SecretController {
     @Autowired
     private SecretService secretService;
 
+    @ExceptionHandler(DataNotExistException.class)
+    public ModelAndView ShowDataNotExistExceptionTip() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("Error/commonError");
+        modelAndView.addObject("ErrorTitle", "树洞信息不存在");
+        modelAndView.addObject("ErrorMessage", "查询的树洞信息不存在");
+        return modelAndView;
+    }
+
     /**
      * 进入校园树洞应用首页
      *
-     * @param request
      * @return
      */
     @RequestMapping(value = {"/secret"}, method = RequestMethod.GET)
-    public ModelAndView ResolveSecretIndexPage(HttpServletRequest request) {
+    public ModelAndView ResolveSecretIndexPage() {
         ModelAndView modelAndView = new ModelAndView();
-        String username = (String) request.getSession().getAttribute("username");
-        BaseResult<List<Secret>, DataBaseResultEnum> result = secretService.GetSecretInfo(0, 10, username);
-        switch (result.getResultType()) {
-            case SUCCESS:
-                modelAndView.addObject("SecretList", result.getResultData());
-                modelAndView.setViewName("Secret/secretIndex");
-                break;
-
-            case EMPTY_RESULT:
-                modelAndView.setViewName("Secret/secretIndex");
-                break;
-
-            case ERROR:
-                modelAndView.setViewName("Error/commonError");
-                modelAndView.addObject("ErrorTitle", "广东第二师范学院树洞-错误");
-                modelAndView.addObject("ErrorMessage", "服务器异常，请稍候再试");
-                break;
-        }
+        modelAndView.setViewName("Secret/secretIndex");
         return modelAndView;
     }
 
     /**
      * 进入树洞信息发布界面
      *
-     * @param request
      * @return
      */
     @RequestMapping(value = {"/secret/publish"}, method = RequestMethod.GET)
-    public ModelAndView ResolveSecretPublishPage(HttpServletRequest request) {
+    public ModelAndView ResolveSecretPublishPage() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("Secret/secretPublish");
         return modelAndView;
@@ -75,26 +60,12 @@ public class SecretController {
      * @return
      */
     @RequestMapping(value = {"/secret/profile"}, method = RequestMethod.GET)
-    public ModelAndView ResolveSecretProfilePage(HttpServletRequest request) {
+    public ModelAndView ResolveSecretProfilePage(HttpServletRequest request) throws Exception {
         ModelAndView modelAndView = new ModelAndView();
         String username = (String) request.getSession().getAttribute("username");
-        BaseResult<List<Secret>, DataBaseResultEnum> result = secretService.GetSecretInfo(username);
-        switch (result.getResultType()) {
-            case SUCCESS:
-                modelAndView.addObject("SecretList", result.getResultData());
-                modelAndView.setViewName("Secret/secretProfile");
-                break;
-
-            case EMPTY_RESULT:
-                modelAndView.setViewName("Secret/secretProfile");
-                break;
-
-            case ERROR:
-                modelAndView.setViewName("Error/commonError");
-                modelAndView.addObject("ErrorTitle", "广东第二师范学院树洞-错误");
-                modelAndView.addObject("ErrorMessage", "服务器异常，请稍候再试");
-                break;
-        }
+        List<Secret> secretList = secretService.GetSecretInfo(username);
+        modelAndView.addObject("SecretList", secretList);
+        modelAndView.setViewName("Secret/secretProfile");
         return modelAndView;
     }
 
@@ -105,186 +76,12 @@ public class SecretController {
      * @return
      */
     @RequestMapping(value = {"/secret/detail/id/{id}"}, method = RequestMethod.GET)
-    public ModelAndView ResolveSecretDetailPage(HttpServletRequest request, @PathVariable("id") int id) {
+    public ModelAndView ResolveSecretDetailPage(HttpServletRequest request, @PathVariable("id") int id) throws Exception {
         ModelAndView modelAndView = new ModelAndView();
         String username = (String) request.getSession().getAttribute("username");
-        BaseResult<Secret, DataBaseResultEnum> result = secretService.GetSecretDetailInfo(id, username);
-        switch (result.getResultType()) {
-            case SUCCESS:
-                modelAndView.setViewName("Secret/secretDetail");
-                modelAndView.addObject("Secret", result.getResultData());
-                break;
-
-            case EMPTY_RESULT:
-                modelAndView.setViewName("Error/commonError");
-                modelAndView.addObject("ErrorTitle", "广东第二师范学院树洞-错误");
-                modelAndView.addObject("ErrorMessage", "树洞信息不存在");
-                break;
-
-            case ERROR:
-                modelAndView.setViewName("Error/commonError");
-                modelAndView.addObject("ErrorTitle", "广东第二师范学院树洞-错误");
-                modelAndView.addObject("ErrorMessage", "服务器异常，请稍候再试");
-                break;
-        }
+        Secret secret = secretService.GetSecretDetailInfo(id, username);
+        modelAndView.setViewName("Secret/secretDetail");
+        modelAndView.addObject("Secret", secret);
         return modelAndView;
-    }
-
-    /**
-     * 获取更多的树洞消息
-     *
-     * @param start
-     * @return
-     */
-    @RequestMapping(value = "/rest/secret/info/start/{start}", method = RequestMethod.GET)
-    @ResponseBody
-    public DataJsonResult<List<Secret>> GetMoreSecret(HttpServletRequest request, @PathVariable("start") int start) {
-        DataJsonResult<List<Secret>> result = new DataJsonResult<>();
-        String username = (String) request.getSession().getAttribute("username");
-        if (username == null || username.trim().isEmpty()) {
-            result.setSuccess(false);
-            result.setMessage("用户凭证已过期，请重新登录");
-        } else {
-            BaseResult<List<Secret>, DataBaseResultEnum> queryResult = secretService.GetSecretInfo(start, 10, username);
-            switch (queryResult.getResultType()) {
-                case SUCCESS:
-                    result.setSuccess(true);
-                    result.setData(queryResult.getResultData());
-                    break;
-
-                case EMPTY_RESULT:
-                    result.setSuccess(true);
-                    break;
-
-                case ERROR:
-                    result.setSuccess(false);
-                    result.setMessage("服务器异常，请稍候再试");
-                    break;
-            }
-        }
-        return result;
-    }
-
-    /**
-     * 发布树洞消息
-     *
-     * @param request
-     * @param secret
-     * @return
-     */
-    @RequestMapping(value = "/rest/secret/info", method = RequestMethod.POST)
-    @ResponseBody
-    public JsonResult AddSecretInfo(HttpServletRequest request, @Validated Secret secret
-            , BindingResult bindingResult) {
-        JsonResult result = new JsonResult();
-        if (bindingResult.hasErrors()) {
-            result.setSuccess(false);
-            result.setMessage("请求参数不合法");
-            return result;
-        }
-        String username = (String) request.getSession().getAttribute("username");
-        if (username == null || username.trim().isEmpty()) {
-            result.setSuccess(false);
-            result.setMessage("用户凭证已过期，请重新登录");
-        } else {
-            boolean addSecretResult = secretService.AddSecretInfo(username, secret);
-            if (addSecretResult) {
-                result.setSuccess(true);
-            } else {
-                result.setSuccess(false);
-                result.setMessage("服务器异常，请稍候再试");
-            }
-        }
-        return result;
-    }
-
-    /**
-     * 添加树洞评论
-     *
-     * @param request
-     * @param id
-     * @param comment
-     * @return
-     */
-    @RequestMapping(value = "/rest/secret/id/{id}/comment", method = RequestMethod.POST)
-    @ResponseBody
-    public JsonResult AddSecretComment(HttpServletRequest request, @PathVariable("id") int id, String comment) {
-        JsonResult result = new JsonResult();
-        if (comment == null || comment.trim().isEmpty() || comment.length() > 50) {
-            result.setSuccess(false);
-            result.setMessage("请求参数不合法");
-            return result;
-        }
-        String username = (String) request.getSession().getAttribute("username");
-        if (username == null || username.trim().isEmpty()) {
-            result.setSuccess(false);
-            result.setMessage("用户凭证已过期，请重新登录");
-        } else {
-            if (!secretService.CheckSecretInfoExist(id)) {
-                result.setSuccess(false);
-                result.setMessage("该树洞消息不存在");
-            } else {
-                if (secretService.AddSecretComment(id, username, comment)) {
-                    result.setSuccess(true);
-                } else {
-                    result.setSuccess(false);
-                    result.setMessage("服务器异常，请稍候再试");
-                }
-            }
-        }
-        return result;
-    }
-
-    /**
-     * 更改点赞状态
-     *
-     * @param request
-     * @param id
-     * @param like
-     * @return
-     */
-    @RequestMapping(value = "/rest/secret/id/{id}/like", method = RequestMethod.POST)
-    @ResponseBody
-    public JsonResult UpdateSecretLikeState(HttpServletRequest request
-            , @PathVariable("id") int id, int like) {
-        JsonResult result = new JsonResult();
-        String username = (String) request.getSession().getAttribute("username");
-        if (username == null || username.trim().isEmpty()) {
-            result.setSuccess(false);
-            result.setMessage("用户凭证已过期，请重新登录");
-        } else {
-            if (secretService.CheckSecretInfoExist(id)) {
-                switch (like) {
-                    case 0:
-                        //取消点赞
-                        if (secretService.ChangeUserLikeState(false, id, username)) {
-                            result.setSuccess(true);
-                        } else {
-                            result.setSuccess(false);
-                            result.setMessage("服务器异常，请稍候再试");
-                        }
-                        break;
-
-                    case 1:
-                        //点赞
-                        if (secretService.ChangeUserLikeState(true, id, username)) {
-                            result.setSuccess(true);
-                        } else {
-                            result.setSuccess(false);
-                            result.setMessage("服务器异常，请稍候再试");
-                        }
-                        break;
-
-                    default:
-                        result.setSuccess(false);
-                        result.setMessage("请求参数不合法");
-                        break;
-                }
-            } else {
-                result.setSuccess(false);
-                result.setMessage("该树洞信息不存在");
-            }
-        }
-        return result;
     }
 }
---
+++ src/main/java/com/linguancheng/gdeiassistant/Controller/Secret/SecretRestController.java
@@ -0,0 +1,115 @@
+package com.linguancheng.gdeiassistant.Controller.Secret;
+
+import com.linguancheng.gdeiassistant.Exception.DatabaseException.DataNotExistException;
+import com.linguancheng.gdeiassistant.Pojo.Entity.Secret;
+import com.linguancheng.gdeiassistant.Pojo.Result.DataJsonResult;
+import com.linguancheng.gdeiassistant.Pojo.Result.JsonResult;
+import com.linguancheng.gdeiassistant.Service.Secret.SecretService;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * 广东二师助手团队 林冠成 版权所有
+ * All rights reserved © 2016 - 2018
+ * Author:林冠成
+ * Date:2018/11/24
+ */
+
+@RestController
+public class SecretRestController {
+
+    @Autowired
+    private SecretService secretService;
+
+    @ExceptionHandler(DataNotExistException.class)
+    public ResponseEntity ShowDataNotExistExceptionTip() {
+        return ResponseEntity.ok(new JsonResult(false
+                , "查询的校园树洞信息不存在"));
+    }
+
+    /**
+     * 获取更多的树洞消息
+     *
+     * @param start
+     * @return
+     */
+    @RequestMapping(value = "/api/secret/info/start/{start}/size/{size}", method = RequestMethod.GET)
+    public DataJsonResult<List<Secret>> GetMoreSecret(HttpServletRequest request
+            , @PathVariable("start") int start, @PathVariable("size") int size) throws Exception {
+        String username = (String) request.getSession().getAttribute("username");
+        List<Secret> secretList = secretService.GetSecretInfo(start, size, username);
+        return new DataJsonResult<>(true, secretList);
+    }
+
+    /**
+     * 发布树洞消息
+     *
+     * @param request
+     * @param secret
+     * @return
+     */
+    @RequestMapping(value = "/api/secret/info", method = RequestMethod.POST)
+    public JsonResult AddSecretInfo(HttpServletRequest request, @Validated Secret secret) throws Exception {
+        String username = (String) request.getSession().getAttribute("username");
+        secretService.AddSecretInfo(username, secret);
+        return new JsonResult(true);
+    }
+
+    /**
+     * 添加树洞评论
+     *
+     * @param request
+     * @param id
+     * @param comment
+     * @return
+     */
+    @RequestMapping(value = "/api/secret/id/{id}/comment", method = RequestMethod.POST)
+    public JsonResult AddSecretComment(HttpServletRequest request, @PathVariable("id") int id
+            , @Validated @NotBlank @Length(min = 1, max = 50) String comment) throws Exception {
+        String username = (String) request.getSession().getAttribute("username");
+        if (!secretService.CheckSecretInfoExist(id)) {
+            throw new DataNotExistException("查询的校园树洞信息不存在");
+        }
+        secretService.AddSecretComment(id, username, comment);
+        return new JsonResult(true);
+    }
+
+    /**
+     * 更改点赞状态
+     *
+     * @param request
+     * @param id
+     * @param like
+     * @return
+     */
+    @RequestMapping(value = "/api/secret/id/{id}/like", method = RequestMethod.POST)
+    public JsonResult UpdateSecretLikeState(HttpServletRequest request
+            , @PathVariable("id") int id, @Validated @Range(min = 0, max = 1) int like) throws Exception {
+        String username = (String) request.getSession().getAttribute("username");
+        if (secretService.CheckSecretInfoExist(id)) {
+            switch (like) {
+                case 0:
+                    //取消点赞
+                    secretService.ChangeUserLikeState(false, id, username);
+                    return new JsonResult(true);
+
+                case 1:
+                    //点赞
+                    secretService.ChangeUserLikeState(true, id, username);
+                    return new JsonResult(true);
+
+                default:
+                    return new JsonResult(false, "请求参数不合法");
+            }
+        }
+        throw new DataNotExistException("查询的校园树洞信息不存在");
+    }
+}

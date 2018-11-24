150bb02b61e282a361b0e2c8433aeb31c1f3d3b6
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-11-25 01:42:02 +0800

    使用统一异常处理替代错误枚举类型

--- src/main/java/com/linguancheng/gdeiassistant/Service/Secret/SecretService.java
+++ src/main/java/com/linguancheng/gdeiassistant/Service/Secret/SecretService.java
@@ -1,11 +1,10 @@
 package com.linguancheng.gdeiassistant.Service.Secret;
 
-import com.linguancheng.gdeiassistant.Enum.Base.DataBaseResultEnum;
+import com.linguancheng.gdeiassistant.Exception.DatabaseException.DataNotExistException;
 import com.linguancheng.gdeiassistant.Repository.Mysql.GdeiAssistant.Secret.SecretMapper;
 import com.linguancheng.gdeiassistant.Pojo.Entity.Secret;
 import com.linguancheng.gdeiassistant.Pojo.Entity.SecretComment;
 import com.linguancheng.gdeiassistant.Pojo.Entity.SecretContent;
-import com.linguancheng.gdeiassistant.Pojo.Result.BaseResult;
 import com.linguancheng.gdeiassistant.Tools.StringEncryptUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -22,8 +22,6 @@ public class SecretService {
     @Resource(name = "secretMapper")
     private SecretMapper secretMapper;
 
-    private Log log = LogFactory.getLog(SecretService.class);
-
     /**
      * 获取树洞消息
      *
@@ -31,27 +29,18 @@ public class SecretService {
      * @param size
      * @return
      */
-    public BaseResult<List<Secret>, DataBaseResultEnum> GetSecretInfo(int start, int size, String username) {
-        BaseResult<List<Secret>, DataBaseResultEnum> result = new BaseResult<>();
-        try {
-            List<Secret> secretList = secretMapper.selectSecret(start, size);
-            if (secretList == null || secretList.isEmpty()) {
-                result.setResultType(DataBaseResultEnum.EMPTY_RESULT);
-            } else {
-                for (Secret secret : secretList) {
-                    //加载点赞数量/评论数量/点赞状态
-                    secret.setCommentCount(secretMapper.selectSecretCommentCount(secret.getId()));
-                    secret.setLikeCount(secretMapper.selectSecretLikeCount(secret.getId()));
-                    secret.setLiked(secretMapper.selectSecretLike(secret.getId(), StringEncryptUtils.encryptString(username)));
-                }
-                result.setResultData(secretList);
-                result.setResultType(DataBaseResultEnum.SUCCESS);
-            }
-        } catch (Exception e) {
-            log.error("获取树洞信息异常：" , e);
-            result.setResultType(DataBaseResultEnum.ERROR);
+    public List<Secret> GetSecretInfo(int start, int size, String username) throws Exception {
+        List<Secret> secretList = secretMapper.selectSecret(start, size);
+        if (secretList == null || secretList.isEmpty()) {
+            return new ArrayList<>();
         }
-        return result;
+        for (Secret secret : secretList) {
+            //加载点赞数量/评论数量/点赞状态
+            secret.setCommentCount(secretMapper.selectSecretCommentCount(secret.getId()));
+            secret.setLikeCount(secretMapper.selectSecretLikeCount(secret.getId()));
+            secret.setLiked(secretMapper.selectSecretLike(secret.getId(), StringEncryptUtils.encryptString(username)));
+        }
+        return secretList;
     }
 
     /**
@@ -60,21 +49,12 @@ public class SecretService {
      * @param username
      * @return
      */
-    public BaseResult<List<Secret>, DataBaseResultEnum> GetSecretInfo(String username) {
-        BaseResult<List<Secret>, DataBaseResultEnum> result = new BaseResult<>();
-        try {
-            List<Secret> secretList = secretMapper.selectSecretByUsername(StringEncryptUtils.encryptString(username));
-            if (secretList == null || secretList.isEmpty()) {
-                result.setResultType(DataBaseResultEnum.EMPTY_RESULT);
-            } else {
-                result.setResultData(secretList);
-                result.setResultType(DataBaseResultEnum.SUCCESS);
-            }
-        } catch (Exception e) {
-            log.error("获取用户发布树洞信息异常：" , e);
-            result.setResultType(DataBaseResultEnum.ERROR);
+    public List<Secret> GetSecretInfo(String username) throws Exception {
+        List<Secret> secretList = secretMapper.selectSecretByUsername(StringEncryptUtils.encryptString(username));
+        if (secretList == null || secretList.isEmpty()) {
+            return new ArrayList<>();
         }
-        return result;
+        return secretList;
     }
 
     /**
@@ -83,14 +63,9 @@ public class SecretService {
      * @param id
      * @return
      */
-    public boolean CheckSecretInfoExist(int id) {
-        try {
-            Secret secret = secretMapper.selectSecretByID(id);
-            return secret != null;
-        } catch (Exception e) {
-            log.error("ID查询树洞信息异常：" , e);
-            return false;
-        }
+    public boolean CheckSecretInfoExist(int id) throws Exception {
+        Secret secret = secretMapper.selectSecretByID(id);
+        return secret != null;
     }
 
     /**
@@ -99,27 +74,18 @@ public class SecretService {
      * @param id
      * @return
      */
-    public BaseResult<Secret, DataBaseResultEnum> GetSecretDetailInfo(int id, String username) {
-        BaseResult<Secret, DataBaseResultEnum> result = new BaseResult<>();
-        try {
-            Secret secret = secretMapper.selectSecretByID(id);
-            if (secret != null) {
-                //加载关联的评论数据
-                secret.getSecretCommentList();
-                //加载点赞数量/评论数量/点赞状态
-                secret.setCommentCount(secretMapper.selectSecretCommentCount(secret.getId()));
-                secret.setLikeCount(secretMapper.selectSecretLikeCount(secret.getId()));
-                secret.setLiked(secretMapper.selectSecretLike(secret.getId(), StringEncryptUtils.encryptString(username)));
-                result.setResultData(secret);
-                result.setResultType(DataBaseResultEnum.SUCCESS);
-            } else {
-                result.setResultType(DataBaseResultEnum.EMPTY_RESULT);
-            }
-        } catch (Exception e) {
-            log.error("获取树洞详细信息异常：" , e);
-            result.setResultType(DataBaseResultEnum.ERROR);
+    public Secret GetSecretDetailInfo(int id, String username) throws Exception {
+        Secret secret = secretMapper.selectSecretByID(id);
+        if (secret != null) {
+            //加载关联的评论数据
+            secret.getSecretCommentList();
+            //加载点赞数量/评论数量/点赞状态
+            secret.setCommentCount(secretMapper.selectSecretCommentCount(secret.getId()));
+            secret.setLikeCount(secretMapper.selectSecretLikeCount(secret.getId()));
+            secret.setLiked(secretMapper.selectSecretLike(secret.getId(), StringEncryptUtils.encryptString(username)));
+            return secret;
         }
-        return result;
+        throw new DataNotExistException("查询的树洞消息不存在");
     }
 
     /**
@@ -129,15 +95,9 @@ public class SecretService {
      * @param secret
      * @return
      */
-    public boolean AddSecretInfo(String username, Secret secret) {
-        try {
-            SecretContent secretContent = new SecretContent(secret, StringEncryptUtils.encryptString(username));
-            secretMapper.insertSecret(secretContent);
-            return true;
-        } catch (Exception e) {
-            log.error("添加树洞信息异常：" , e);
-            return false;
-        }
+    public void AddSecretInfo(String username, Secret secret) throws Exception {
+        SecretContent secretContent = new SecretContent(secret, StringEncryptUtils.encryptString(username));
+        secretMapper.insertSecret(secretContent);
     }
 
     /**
@@ -148,20 +108,14 @@ public class SecretService {
      * @param comment
      * @return
      */
-    public boolean AddSecretComment(int id, String username, String comment) {
-        try {
-            SecretComment secretComment = new SecretComment();
-            secretComment.setId(id);
-            secretComment.setUsername(StringEncryptUtils.encryptString(username));
-            secretComment.setComment(comment);
-            secretComment.setAvatarTheme((int) (Math.random() * 50));
-            secretComment.setPublishTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-            secretMapper.insertSecretComment(secretComment);
-            return true;
-        } catch (Exception e) {
-            log.error("添加树洞评论异常：" , e);
-            return false;
-        }
+    public void AddSecretComment(int id, String username, String comment) throws Exception {
+        SecretComment secretComment = new SecretComment();
+        secretComment.setId(id);
+        secretComment.setUsername(StringEncryptUtils.encryptString(username));
+        secretComment.setComment(comment);
+        secretComment.setAvatarTheme((int) (Math.random() * 50));
+        secretComment.setPublishTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        secretMapper.insertSecretComment(secretComment);
     }
 
     /**
@@ -172,20 +126,13 @@ public class SecretService {
      * @param username
      * @return
      */
-    public boolean ChangeUserLikeState(boolean like, int id, String username) {
-        try {
-            if (like) {
-                //点赞
-                secretMapper.insertSecretLike(id, StringEncryptUtils.encryptString(username));
-            } else {
-                //取消点赞
-                secretMapper.deleteSecretLike(id, StringEncryptUtils.encryptString(username));
-            }
-            return true;
-        } catch (Exception e) {
-            log.error("更改树洞点赞状态异常：" , e);
-            return false;
+    public void ChangeUserLikeState(boolean like, int id, String username) throws Exception {
+        if (like) {
+            //点赞
+            secretMapper.insertSecretLike(id, StringEncryptUtils.encryptString(username));
+        } else {
+            //取消点赞
+            secretMapper.deleteSecretLike(id, StringEncryptUtils.encryptString(username));
         }
     }
-
 }

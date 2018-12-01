5e8a70860da7eb14050dcbda133846b5c320997e
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-12-02 05:49:21 +0800

    自定义课程异常类

---
+++ src/main/java/com/linguancheng/gdeiassistant/Exception/CustomScheduleException/CountOverLimitException.java
@@ -0,0 +1,11 @@
+package com.linguancheng.gdeiassistant.Exception.CustomScheduleException;
+
+public class CountOverLimitException extends Exception {
+
+    public CountOverLimitException() {
+    }
+
+    public CountOverLimitException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/linguancheng/gdeiassistant/Exception/CustomScheduleException/GenerateScheduleException.java
@@ -0,0 +1,11 @@
+package com.linguancheng.gdeiassistant.Exception.CustomScheduleException;
+
+public class GenerateScheduleException extends Exception {
+
+    public GenerateScheduleException() {
+    }
+
+    public GenerateScheduleException(String message) {
+        super(message);
+    }
+}

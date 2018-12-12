f1862f8bd862167a630b211d1b2c0ee8cc20c691
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-12-12 13:37:49 +0800

    移除获取用户真实姓名接口

--- src/main/java/com/linguancheng/gdeiassistant/Controller/UserProfile/ProfileRestController.java
+++ src/main/java/com/linguancheng/gdeiassistant/Controller/UserProfile/ProfileRestController.java
@@ -1,12 +1,10 @@
 package com.linguancheng.gdeiassistant.Controller.UserProfile;
 
 import com.linguancheng.gdeiassistant.Annotation.RestAuthentication;
-import com.linguancheng.gdeiassistant.Exception.CommonException.ServerErrorException;
 import com.linguancheng.gdeiassistant.Pojo.Entity.*;
 import com.linguancheng.gdeiassistant.Pojo.Profile.LocationComparator;
 import com.linguancheng.gdeiassistant.Pojo.Result.DataJsonResult;
 import com.linguancheng.gdeiassistant.Pojo.Result.JsonResult;
-import com.linguancheng.gdeiassistant.Service.Profile.RealNameService;
 import com.linguancheng.gdeiassistant.Service.Profile.UserProfileService;
 import com.linguancheng.gdeiassistant.Tools.LocationUtils;
 import com.linguancheng.gdeiassistant.Tools.StringUtils;
@@ -30,9 +28,6 @@ public class ProfileRestController {
 
     @Autowired
     private UserProfileService userProfileService;
-
-    @Autowired
-    private RealNameService realNameService;
 
     private final int AVATAR_MAX_SIZE = 1024 * 1024 * 2;
 
@@ -365,22 +360,5 @@ public class ProfileRestController {
             jsonResult.setData(url);
         }
         return jsonResult;
-    }
-
-    /**
-     * 获取用户真实姓名
-     *
-     * @param request
-     * @param token
-     * @return
-     */
-    @RequestMapping(value = "/rest/profile/realname", method = RequestMethod.POST)
-    @RestAuthentication
-    public DataJsonResult<String> getUserRealName(HttpServletRequest request
-            , @RequestParam("token") String token) throws ServerErrorException {
-        User user = (User) request.getAttribute("user");
-        String realName = realNameService.GetUserRealName(request.getSession().getId()
-                , user.getUsername(), user.getPassword());
-        return new DataJsonResult<>(true, realName);
     }
 }

d1549e3d7e27b1c12ca78a9b18cafc7edf629e69
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-12-12 13:32:11 +0800

    实名认证类型枚举类型

---
+++ src/main/java/com/linguancheng/gdeiassistant/Converter/StringToAuthenticationTypeEnumConverter.java
@@ -0,0 +1,17 @@
+package com.linguancheng.gdeiassistant.Converter;
+
+import com.linguancheng.gdeiassistant.Enum.Authentication.AuthenticationTypeEnum;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToAuthenticationTypeEnumConverter implements Converter<String, AuthenticationTypeEnum> {
+
+    @Override
+    public AuthenticationTypeEnum convert(String source) {
+        for (AuthenticationTypeEnum authenticationTypeEnum : AuthenticationTypeEnum.values()) {
+            if (authenticationTypeEnum.getType().equals(source)) {
+                return authenticationTypeEnum;
+            }
+        }
+        return null;
+    }
+}

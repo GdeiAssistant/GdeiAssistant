81ba36d196808810bc18d916667b35ee060609e5
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2018-12-12 13:32:11 +0800

    实名认证类型枚举类型

---
+++ src/main/java/com/linguancheng/gdeiassistant/Converter/StringToAuthenticationTypeEnumConverter.java
@@ -0,0 +1,17 @@
+package com.gdeiassistant.gdeiassistant.Converter;
+
+import com.gdeiassistant.gdeiassistant.Enum.Authentication.AuthenticationTypeEnum;
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToAuthenticationTypeEnumConverter implements Converter<String, AuthenticationTypeEnum> {
+
+    @Override
+    public AuthenticationTypeEnum convert(String source) {
+        for (AuthenticationTypeEnum authenticationTypeEnum : AuthenticationTypeEnum.values()) {
+            if (authenticationTypeEnum.getType().equals(source)) {
+                return authenticationTypeEnum;
+            }
+        }
+        return null;
+    }
+}

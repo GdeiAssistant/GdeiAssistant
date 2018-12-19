82df9206b77a65609145eaba3e82592deebb4cfd
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-12-20 04:09:14 +0800

    提供获取RSA公钥的API接口

---
+++ src/main/java/com/linguancheng/gdeiassistant/Controller/RSAEncryption/RSAEncryptionRestController.java
@@ -0,0 +1,23 @@
+package com.linguancheng.gdeiassistant.Controller.RSAEncryption;
+
+import com.linguancheng.gdeiassistant.Pojo.Result.DataJsonResult;
+import com.linguancheng.gdeiassistant.Tools.RSAUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RSAEncryptionRestController {
+
+    /**
+     * 获取RSA公钥密钥Base64编码字符串
+     *
+     * @return
+     */
+    @RequestMapping(value = "/rest/encryption/rsa/publickey", method = RequestMethod.GET)
+    public DataJsonResult<String> GetRSAPublicKey() {
+        return new DataJsonResult<>(true, RSAUtils.GetPublicKey());
+    }
+
+
+}

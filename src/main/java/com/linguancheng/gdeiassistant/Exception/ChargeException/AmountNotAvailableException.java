4aa7d31f6861f4d0bc177bfd59e345f6c44dc831
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-12-30 23:18:59 +0800

    调整校园卡充值异常类型，删除姓名校验、不支持客户端、不支持安全版本异常类，添加安全校验不通过异常类，修改充值金额不合法异常类名称

--- src/main/java/com/linguancheng/gdeiassistant/Exception/ChargeException/AmountNotAvailableException.java
+++ src/main/java/com/linguancheng/gdeiassistant/Exception/ChargeException/AmountNotAvailableException.java
@@ -3,13 +3,13 @@ package com.linguancheng.gdeiassistant.Exception.ChargeException;
 /**
  * 用户充值金额超过范围时抛出该异常
  */
-public class AccountNotAvailableException extends Exception {
+public class AmountNotAvailableException extends Exception {
 
-    public AccountNotAvailableException() {
+    public AmountNotAvailableException() {
         super();
     }
 
-    public AccountNotAvailableException(String message) {
+    public AmountNotAvailableException(String message) {
         super(message);
     }
 }

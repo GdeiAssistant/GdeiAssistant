f5620ba9e67d60ea2491d2c4c837416b0a3cc7bf
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2018-12-31 02:12:15 +0800

    修改实名认证控制器文件夹名称

--- src/main/java/com/linguancheng/gdeiassistant/Controller/Authenticate/AuthenticateController.java
+++ src/main/java/com/linguancheng/gdeiassistant/Controller/Authenticate/AuthenticateController.java
@@ -1,4 +1,4 @@
-package com.linguancheng.gdeiassistant.Controller.RealNameAuthenticate;
+package com.linguancheng.gdeiassistant.Controller.Authenticate;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

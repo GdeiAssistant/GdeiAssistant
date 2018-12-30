d8ad8fcd0f83599730d4603e5ce74eb7637c2723
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2018-12-31 02:12:15 +0800

    修改实名认证控制器文件夹名称

--- src/main/java/com/linguancheng/gdeiassistant/Controller/Authenticate/AuthenticateController.java
+++ src/main/java/com/linguancheng/gdeiassistant/Controller/Authenticate/AuthenticateController.java
@@ -1,4 +1,4 @@
-package com.gdeiassistant.gdeiassistant.Controller.RealNameAuthenticate;
+package com.gdeiassistant.gdeiassistant.Controller.Authenticate;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

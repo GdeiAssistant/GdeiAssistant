ccd638b311299fcb1e667c055e084374d73a25ee
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-01-06 04:14:02 +0800

    移除自定义EncryptDataMappingJackson2HttpMessageConverter转换器

--- src/main/java/com/linguancheng/gdeiassistant/Config/WebConfig.java
+++ src/main/java/com/linguancheng/gdeiassistant/Config/WebConfig.java
@@ -1,20 +1,12 @@
 package com.linguancheng.gdeiassistant.Config;
 
-import com.linguancheng.gdeiassistant.Converter.EncryptDataMappingJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.List;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new EncryptDataMappingJackson2HttpMessageConverter());
-        super.configureMessageConverters(converters);
-    }
+    
 }

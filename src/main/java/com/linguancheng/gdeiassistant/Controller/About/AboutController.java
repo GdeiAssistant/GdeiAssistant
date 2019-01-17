388b6746b32f44f64f827b5039a5fd1db841a429
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-01-18 01:22:59 +0800

    客户端下载和协议与政策的功能菜单整合到关于应用中，更新用户隐私政策，添加应用社区准则，删除不再使用的客户端下载和代码开源菜单图标

--- src/main/java/com/linguancheng/gdeiassistant/Controller/About/AboutController.java
+++ src/main/java/com/linguancheng/gdeiassistant/Controller/About/AboutController.java
@@ -1,4 +1,4 @@
-package com.linguancheng.gdeiassistant.Controller.AgreementAndPolicy;
+package com.linguancheng.gdeiassistant.Controller.About;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,20 +6,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class AgreementAndPolicyController {
+public class AboutController {
+
+    @RequestMapping(value = "/about", method = RequestMethod.GET)
+    public ModelAndView ResolveAboutPage() {
+        return new ModelAndView("About/index");
+    }
 
     @RequestMapping(value = "/agreement", method = RequestMethod.GET)
     public ModelAndView ResolveAgreementPage() {
-        return new ModelAndView("AgreementAndPolicy/agreement");
+        return new ModelAndView("About/agreement");
     }
 
     @RequestMapping(value = "/policy/cookie", method = RequestMethod.GET)
     public ModelAndView ResolveCookiePolicyPage() {
-        return new ModelAndView("AgreementAndPolicy/cookiePolicy");
+        return new ModelAndView("About/cookiePolicy");
     }
 
     @RequestMapping(value = "/policy/privacy", method = RequestMethod.GET)
     public ModelAndView ResolvePrivacyPolicyPage() {
-        return new ModelAndView("AgreementAndPolicy/privacyPolicy");
+        return new ModelAndView("About/privacyPolicy");
+    }
+
+    @RequestMapping(value = "/policy/social", method = RequestMethod.GET)
+    public ModelAndView ResolveSocialPolicyPage() {
+        return new ModelAndView("About/socialPolicy");
     }
 }

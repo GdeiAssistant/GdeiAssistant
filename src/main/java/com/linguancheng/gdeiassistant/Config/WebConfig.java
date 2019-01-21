0856bb07f4715d75f64ee08d2c3ee158b06192a6
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-01-21 08:25:39 +0800

    添加WechatMappingJackson2HttpMessageConverter，添加对TEXT和HTML媒体类型的支持

--- src/main/java/com/linguancheng/gdeiassistant/Config/WebConfig.java
+++ src/main/java/com/linguancheng/gdeiassistant/Config/WebConfig.java
@@ -1,6 +1,10 @@
 package com.linguancheng.gdeiassistant.Config;
 
+import com.linguancheng.gdeiassistant.Converter.HttpMessageConvert.WeChatMappingJackson2HttpMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -8,5 +12,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-    
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new WeChatMappingJackson2HttpMessageConverter());
+        return restTemplate;
+    }
+
+    @Bean
+    public AsyncRestTemplate asyncRestTemplate() {
+        AsyncRestTemplate asyncRestTemplate = new AsyncRestTemplate();
+        asyncRestTemplate.getMessageConverters().add(new WeChatMappingJackson2HttpMessageConverter());
+        return asyncRestTemplate;
+    }
 }

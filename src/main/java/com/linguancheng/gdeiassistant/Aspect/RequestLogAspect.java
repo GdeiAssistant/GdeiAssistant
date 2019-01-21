03770e6ff0e103e9b994ec4f5f49ff272d0ed9b0
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2019-01-22 02:27:28 +0800

    使用FastJson记录请求参数序列化的字符串结果

--- src/main/java/com/linguancheng/gdeiassistant/Aspect/RequestLogAspect.java
+++ src/main/java/com/linguancheng/gdeiassistant/Aspect/RequestLogAspect.java
@@ -1,5 +1,6 @@
 package com.gdeiassistant.gdeiassistant.Aspect;
 
+import com.alibaba.fastjson.JSON;
 import com.gdeiassistant.gdeiassistant.Tools.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -50,7 +51,7 @@ public class RequestLogAspect {
                     stringBuilder.append(" , ");
                 }
                 stringBuilder.append(parameterName[i])
-                        .append(":").append(args[i].toString());
+                        .append(":").append(JSON.toJSONString(args[i]));
             }
         }
         stringBuilder.append(" . ");

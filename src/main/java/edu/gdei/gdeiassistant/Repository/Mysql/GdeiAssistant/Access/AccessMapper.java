7361c1e21d4fe47cbf9150a4f3058f1f237b8269
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-03-06 20:37:14 +0800

    合并统一的用户权限接口

--- src/main/java/edu/gdei/gdeiassistant/Repository/Mysql/GdeiAssistant/Access/AccessMapper.java
+++ src/main/java/edu/gdei/gdeiassistant/Repository/Mysql/GdeiAssistant/Access/AccessMapper.java
@@ -4,7 +4,5 @@ import edu.gdei.gdeiassistant.Pojo.Entity.Access;
 
 public interface AccessMapper {
 
-    public Access selectAndroidAccess(Integer groupId) throws Exception;
-
-    public Access selectWechatAccess(Integer groupId) throws Exception;
+    public Access selectAccess(Integer groupId) throws Exception;
 }

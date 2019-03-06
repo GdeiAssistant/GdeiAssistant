83e23308a726b724167cc2abf378c38388fdf73a
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-03-06 20:36:32 +0800

    添加UWP版本、微信公众号和支付宝小程序的选项

--- src/main/java/edu/gdei/gdeiassistant/Controller/ClientDownload/ClientDownloadRestController.java
+++ src/main/java/edu/gdei/gdeiassistant/Controller/ClientDownload/ClientDownloadRestController.java
@@ -1,22 +1,23 @@
 package edu.gdei.gdeiassistant.Controller.ClientDownload;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
-public class ClientDownloadController {
+@RestController
+public class ClientDownloadRestController {
 
     private String androidDownloadURL;
 
     private String androidGooglePlayDownloadURL;
 
     private String androidAmazonDownloadURL;
+
+    private String uwpStoreDownloadURL;
 
     @Value("#{propertiesReader['update.android.gooleplayURL']}")
     public void setAndroidGooglePlayDownloadURL(String androidGooglePlayDownloadURL) {
@@ -33,8 +34,12 @@ public class ClientDownloadController {
         this.androidAmazonDownloadURL = androidAmazonDownloadURL;
     }
 
+    @Value("#{propertiesReader['update.uwp.storeURL']}")
+    public void setUwpStoreDownloadURL(String uwpStoreDownloadURL){
+        this.uwpStoreDownloadURL = uwpStoreDownloadURL;
+    }
+
     @RequestMapping(value = "/download/android", method = RequestMethod.GET)
-    @ResponseBody
     public Map<String, String> GetAndroidDownloadURl() {
         Map<String, String> map = new HashMap<>();
         map.put("url", androidDownloadURL);
@@ -42,7 +47,6 @@ public class ClientDownloadController {
     }
 
     @RequestMapping(value = "/download/android/googleplay", method = RequestMethod.GET)
-    @ResponseBody
     public Map<String, String> GetAndroidGooglePlayDownloadURL() {
         Map<String, String> map = new HashMap<>();
         map.put("url", androidGooglePlayDownloadURL);
@@ -50,10 +54,16 @@ public class ClientDownloadController {
     }
 
     @RequestMapping(value = "/download/android/amazon", method = RequestMethod.GET)
-    @ResponseBody
     public Map<String, String> GetAndroidAmazonDownloadURL() {
         Map<String, String> map = new HashMap<>();
         map.put("url", androidAmazonDownloadURL);
+        return map;
+    }
+
+    @RequestMapping(value = "/download/uwp",method = RequestMethod.GET)
+    public Map<String,String> GetUWPStoreDownloadURL(){
+        Map<String, String> map = new HashMap<>();
+        map.put("url", uwpStoreDownloadURL);
         return map;
     }
 }

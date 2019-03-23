0a89c681305023986b82aa5f155c230d0990de1a
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-03-23 14:34:51 +0800

    移除非必要的注解

--- src/main/java/edu/gdei/gdeiassistant/Controller/CloseAccount/CloseAccountRestController.java
+++ src/main/java/edu/gdei/gdeiassistant/Controller/CloseAccount/CloseAccountRestController.java
@@ -5,7 +5,6 @@ import edu.gdei.gdeiassistant.Service.CloseAccount.CloseAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +23,6 @@ public class CloseAccountRestController {
      * @return
      */
     @RequestMapping(value = "/api/close/submit", method = RequestMethod.POST)
-    @ResponseBody
     public JsonResult CloseAccount(HttpServletRequest request, String password) throws Exception {
         String username = (String) request.getSession().getAttribute("username");
         closeAccountService.CloseAccount(username, password);

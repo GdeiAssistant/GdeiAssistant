e39c88c69a68c081211f28baa4cbc574f35f1b25
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-03-23 14:36:37 +0800

    调整中国、香港、澳门和台湾的显示顺序为固定顺序，其余国家或地区使用中文首字拼音排序

--- src/main/java/edu/gdei/gdeiassistant/Pojo/Profile/LocationComparator.java
+++ src/main/java/edu/gdei/gdeiassistant/Pojo/Profile/LocationComparator.java
@@ -2,7 +2,9 @@ package edu.gdei.gdeiassistant.Pojo.Profile;
 
 import edu.gdei.gdeiassistant.Pojo.Entity.Region;
 
+import java.text.Collator;
 import java.util.Comparator;
+import java.util.Locale;
 
 public class LocationComparator implements Comparator {
 
@@ -10,7 +12,22 @@ public class LocationComparator implements Comparator {
     public int compare(Object o1, Object o2) {
         Region r1 = (Region) o1;
         Region r2 = (Region) o2;
-        return r1.getName().substring(4, r1.getName().length())
-                .compareTo(r2.getName().substring(4, r2.getName().length()));
+        if (r1.getName().contains("中国") && r2.getName().contains("中国")) {
+            if (r1.getName().substring(4).equals("中国")) {
+                return -1;
+            }
+            if (r1.getName().substring(4).equals("中国香港")) {
+                if (!r2.getName().substring(4).equals("中国")) {
+                    return -1;
+                }
+            }
+            if (r1.getName().substring(4).equals("中国澳门")) {
+                if (r2.getName().substring(4).equals("中国台湾")) {
+                    return -1;
+                }
+            }
+            return 0;
+        }
+        return Collator.getInstance(Locale.CHINESE).compare(r1.getName().substring(4), r2.getName().substring(4));
     }
 }

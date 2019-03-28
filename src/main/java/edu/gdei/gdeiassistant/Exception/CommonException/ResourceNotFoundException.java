4e552c567b03292383be6464a952b07a177f19cc
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-03-28 22:18:55 +0800

    添加资源和视图不存在的异常处理

---
+++ src/main/java/edu/gdei/gdeiassistant/Exception/CommonException/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package edu.gdei.gdeiassistant.Exception.CommonException;
+
+public class ResourceNotFoundException extends Exception {
+
+    public ResourceNotFoundException() {
+
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}

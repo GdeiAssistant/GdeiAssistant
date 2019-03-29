c858bc7992942ac8803999ccb86accc57cb47460
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-03-30 00:18:19 +0800

    剔除不使用的人脸识别API

--- src/main/java/edu/gdei/gdeiassistant/Service/CloudAPI/BaiduYunService.java
+++ src/main/java/edu/gdei/gdeiassistant/Service/CloudAPI/BaiduYunService.java
@@ -1,6 +1,5 @@
 package edu.gdei.gdeiassistant.Service.CloudAPI;
 
-import edu.gdei.gdeiassistant.Enum.Recognition.RecognitionEnum;
 import edu.gdei.gdeiassistant.Exception.AuthenticationException.*;
 import edu.gdei.gdeiassistant.Exception.RecognitionException.RecognitionException;
 import edu.gdei.gdeiassistant.Pojo.Entity.Identity;
@@ -132,72 +131,6 @@ public class BaiduYunService {
     }
 
     /**
-     * 注册人脸库信息
-     *
-     * @param username
-     * @param image
-     * @return
-     */
-    public RecognitionEnum FaceSetRegister(String username, String image) throws RecognitionException {
-        String token = GetBaiduAccessToken();
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("uid", username);
-        params.add("group_id", "gdeiassistant");
-        params.add("user_info", username);
-        params.add("action_type", "replace");
-        params.add("image", image);
-        JSONObject jsonObject = restTemplate.postForObject("https://aip.baidubce.com/rest/2.0/face/v2/faceset/user/add?access_token="
-                + token, params, JSONObject.class);
-        if (jsonObject.has("error_code")) {
-            //注册人脸信息错误
-            switch (jsonObject.getInt("error_code")) {
-                case 216402:
-                    //未发现人脸
-                    return RecognitionEnum.FACE_NOT_FOUND;
-
-                default:
-                    //服务器异常
-                    return RecognitionEnum.SERVER_ERROR;
-            }
-        }
-        return RecognitionEnum.SUCCESS;
-    }
-
-    /**
-     * 人脸认证
-     *
-     * @param username
-     * @param image
-     * @return
-     * @throws RecognitionException
-     */
-    public RecognitionEnum FaceVerify(String username, String image) throws RecognitionException {
-        String token = GetBaiduAccessToken();
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("uid", username);
-        params.add("image", image);
-        params.add("group_id", "gdeiassistant");
-        JSONObject jsonObject = restTemplate.postForObject("https://aip.baidubce.com/rest/2.0/face/v2/verify?access_token="
-                + token, params, JSONObject.class);
-        if (jsonObject.has("error_code")) {
-            //注册人脸信息错误
-            switch (jsonObject.getInt("error_code")) {
-                case 216402:
-                    //未发现人脸
-                    return RecognitionEnum.FACE_NOT_FOUND;
-
-                default:
-                    //服务器异常
-                    return RecognitionEnum.SERVER_ERROR;
-            }
-        }
-        if (jsonObject.getJSONArray("result").getInt(0) >= 70) {
-            return RecognitionEnum.SUCCESS;
-        }
-        return RecognitionEnum.FAILURE;
-    }
-
-    /**
      * 高精度识别验证码图片中的文字，返回文本串
      *
      * @param image
@@ -242,8 +175,8 @@ public class BaiduYunService {
         if (jsonObject.has("log_id") && jsonObject.has("words_result")) {
             JSONArray jsonArray = jsonObject.getJSONArray("words_result");
             StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < jsonArray.size(); i++) {
-                String words = ((JSONObject) jsonArray.get(i)).getString("words");
+            for (Object o : jsonArray) {
+                String words = ((JSONObject) o).getString("words");
                 if (words.matches("^[0-9]*$")) {
                     stringBuilder.append(words);
                 }

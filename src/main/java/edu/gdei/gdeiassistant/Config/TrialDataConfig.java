dcda2e65a9798d3d9d783baa27a18550f56eaea2
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2019-05-13 05:59:04 +0800

    使用Properties文件保存各模块的体验用户测试数据
    Former-commit-id: f8a410cb1f8a5e07c959b7c28fc3644982018d07

---
+++ src/main/java/edu/gdei/gdeiassistant/Config/TrialDataConfig.java
@@ -0,0 +1,10 @@
+package edu.gdei.gdeiassistant.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "classpath:/config/trial/data.properties", encoding = "UTF-8")
+public class TrialDataConfig {
+
+}

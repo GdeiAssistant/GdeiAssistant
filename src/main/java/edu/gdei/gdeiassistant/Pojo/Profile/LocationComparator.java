ffd7915727096868dd88fce69ad29356645208f8
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-05-21 00:05:51 +0800

    调整国家/地区名称位置和加载顺序

--- src/main/java/edu/gdei/gdeiassistant/Pojo/Profile/LocationComparator.java
+++ src/main/java/edu/gdei/gdeiassistant/Pojo/Profile/LocationComparator.java
@@ -12,22 +12,6 @@ public class LocationComparator implements Comparator {
     public int compare(Object o1, Object o2) {
         Region r1 = (Region) o1;
         Region r2 = (Region) o2;
-        if (r1.getName().contains("中国") && r2.getName().contains("中国")) {
-            if (r1.getName().substring(4).equals("中国")) {
-                return -1;
-            }
-            if (r1.getName().substring(4).equals("中国香港")) {
-                if (!r2.getName().substring(4).equals("中国")) {
-                    return -1;
-                }
-            }
-            if (r1.getName().substring(4).equals("中国澳门")) {
-                if (r2.getName().substring(4).equals("中国台湾")) {
-                    return -1;
-                }
-            }
-            return 0;
-        }
         return Collator.getInstance(Locale.CHINESE).compare(r1.getName().substring(4), r2.getName().substring(4));
     }
 }

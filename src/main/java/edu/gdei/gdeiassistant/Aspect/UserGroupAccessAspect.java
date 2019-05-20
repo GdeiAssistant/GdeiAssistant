9e63214b9705ffe74f1eefcafca2e0aff76a6149
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2019-05-20 23:35:05 +0800

    添加AOP切面的执行顺序
    Former-commit-id: 496efbadf8514fb535fd6ae879979b4fd716d6f2

--- src/main/java/edu/gdei/gdeiassistant/Aspect/UserGroupAccessAspect.java
+++ src/main/java/edu/gdei/gdeiassistant/Aspect/UserGroupAccessAspect.java
@@ -10,12 +10,14 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Aspect
 @Component
+@Order(1)
 public class UserGroupAccessAspect {
 
     @Autowired

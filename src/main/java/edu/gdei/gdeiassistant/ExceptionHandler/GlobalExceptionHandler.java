7b73c8efafa54b2efd65482d4e598248d463c014
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-05-25 01:29:40 +0800

    HTTP状态码405错误的异常转移到ServletExceptionHandler中进行处理，补全该分类的异常的类值

--- src/main/java/edu/gdei/gdeiassistant/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/edu/gdei/gdeiassistant/ExceptionHandler/GlobalExceptionHandler.java
@@ -3,53 +3,21 @@ package edu.gdei.gdeiassistant.ExceptionHandler;
 import edu.gdei.gdeiassistant.Exception.DatabaseException.DataNotExistException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.MethodNotSupportedException;
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.ConstraintViolationException;
-import java.io.IOException;
 
 @ControllerAdvice(annotations = Controller.class)
 @Order(value = 3)
 public class GlobalExceptionHandler {
 
     private Log log = LogFactory.getLog(GlobalExceptionHandler.class);
-
-    /**
-     * 处理HTTP请求400错误
-     *
-     * @return
-     */
-    @ExceptionHandler({MissingServletRequestParameterException.class
-            , TypeMismatchException.class, HttpMessageNotReadableException.class})
-    public ModelAndView HandleBadRequestException() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setStatus(HttpStatus.BAD_REQUEST);
-        modelAndView.setViewName("Error/badRequestError");
-        return modelAndView;
-    }
-
-    /**
-     * 处理HTTP请求405错误
-     *
-     * @throws IOException
-     */
-    @ExceptionHandler(MethodNotSupportedException.class)
-    public ModelAndView HandleMethodNotSupportedException() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setStatus(HttpStatus.METHOD_NOT_ALLOWED);
-        modelAndView.setViewName("Error/methodNotAllowError");
-        return modelAndView;
-    }
 
     /**
      * 处理数据校验异常

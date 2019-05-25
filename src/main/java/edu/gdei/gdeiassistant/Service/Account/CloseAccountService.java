d7206d03bdd7ad1d65cc6f647560ed6693752cf5
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-05-26 00:02:31 +0800

    修正用户注销账号的逻辑错误，用户账号状态正常的参数值由0修改为1，移除关闭账号SQL语句中User表不存在的字段

--- src/main/java/edu/gdei/gdeiassistant/Service/Account/CloseAccountService.java
+++ src/main/java/edu/gdei/gdeiassistant/Service/Account/CloseAccountService.java
@@ -85,7 +85,7 @@ public class CloseAccountService {
     public void CloseAccount(String username, String password) throws Exception {
         //检查用户账号状态
         User user = userMapper.selectUser(StringEncryptUtils.encryptString(username)).decryptUser();
-        if (user == null || !user.getState().equals(0)) {
+        if (user == null || !user.getState().equals(1)) {
             //若账号状态异常，则抛出异常
             throw new UserStateErrorException("用户账号状态异常");
         }

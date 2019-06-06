906cfd6cd19d7c90282ec4341867b74aed817fbc
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-06-06 13:47:10 +0800

    移除多余的保存专题阅读信息的数据接口和方法

--- src/main/java/edu/gdei/gdeiassistant/Controller/Reading/ReadingRestController.java
+++ src/main/java/edu/gdei/gdeiassistant/Controller/Reading/ReadingRestController.java
@@ -1,17 +1,13 @@
 package edu.gdei.gdeiassistant.Controller.Reading;
 
-import edu.gdei.gdeiassistant.Annotation.UserGroupAccess;
 import edu.gdei.gdeiassistant.Pojo.Entity.Reading;
 import edu.gdei.gdeiassistant.Pojo.Result.DataJsonResult;
-import edu.gdei.gdeiassistant.Pojo.Result.JsonResult;
 import edu.gdei.gdeiassistant.Service.Reading.ReadingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -29,19 +25,5 @@ public class ReadingRestController {
     public DataJsonResult<List<Reading>> LoadReadingList() {
         List<Reading> readingList = readingService.LoadingLatestReadingList();
         return new DataJsonResult<>(true, readingList);
-    }
-
-    /**
-     * 保存专题阅读信息
-     *
-     * @param request
-     * @param reading
-     * @return
-     */
-    @RequestMapping(value = "/api/reading", method = RequestMethod.POST)
-    @UserGroupAccess(group = 1)
-    public JsonResult SaveReadingInfo(HttpServletRequest request, @Validated Reading reading) {
-        readingService.SaveReadingInfo(reading);
-        return new JsonResult(true);
     }
 }

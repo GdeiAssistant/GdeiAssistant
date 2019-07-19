fc95ee54be758adb5ecc94f31de6e0b83d2e90c1
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2019-07-19 14:55:37 +0800

    使用其他命名空间下已定义的ResultMap进行映射
    Former-commit-id: fb8bf794322a8f5121711939bcff54c1f9be2271
    Former-commit-id: 7c21ae5b6f748d555c893bb10e37ccc0b6b9e755
    Former-commit-id: 5aaed51a3d3a8b6d38092210f6d185e5e06ab51d

--- src/main/java/edu/gdei/gdeiassistant/Repository/Mysql/GdeiAssistant/Mapper/Data/AppDataMapper.java
+++ src/main/java/edu/gdei/gdeiassistant/Repository/Mysql/GdeiAssistant/Mapper/Data/AppDataMapper.java
@@ -3,131 +3,52 @@ package edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Data;
 import edu.gdei.gdeiassistant.Pojo.Entity.*;
 import edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.SQLBuilder.DataSQLBuilder;
 import org.apache.ibatis.annotations.*;
-import org.apache.ibatis.type.JdbcType;
 
-import java.util.Date;
 import java.util.List;
 
 public interface AppDataMapper {
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserProfile")
-    @Results(id = "Profile", value = {
-            @Result(property = "kickname", column = "kickname"),
-            @Result(property = "gender", column = "gender"),
-            @Result(property = "customGenderName", column = "custom_gender"),
-            @Result(property = "birthday", column = "birthday"),
-            @Result(property = "degree", column = "degree"),
-            @Result(property = "faculty", column = "faculty"),
-            @Result(property = "major", column = "major"),
-            @Result(property = "region", column = "region"),
-            @Result(property = "state", column = "state"),
-            @Result(property = "city", column = "city")
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Profile.ProfileMapper.Profile")
     public Profile selectUserProfile(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserIntroduction")
-    @Results(id = "introduction", value = {
-            @Result(property = "introductionContent", column = "introduction")
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Profile.ProfileMapper.Introduction")
     public Introduction selectUserIntroduction(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserPrivacy")
-    @Results(id = "Privacy", value = {
-            @Result(property = "genderOpen", column = "is_gender_open"),
-            @Result(property = "regionOpen", column = "is_region_open"),
-            @Result(property = "introductionOpen", column = "is_introduction_open"),
-            @Result(property = "facultyOpen", column = "is_faculty_open"),
-            @Result(property = "majorOpen", column = "is_major_open"),
-            @Result(property = "cacheAllow", column = "is_cache_allow"),
-            @Result(property = "robotsIndexAllow", column = "is_robots_index_allow")
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Privacy.PrivacyMapper.Privacy")
     public Privacy selectUserPrivacy(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserGraduation")
-    @Results(id = "Graduation", value = {
-            @Result(property = "program", column = "program")
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Graduation.GraduationMapper.Graduation")
     public Graduation selectUserGraduation(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserCetNumber")
-    @Results(id = "CetNumber", value = {
-            @Result(property = "number", column = "number")
-    })
+    @ResultType(CetNumber.class)
     public CetNumber selectUserCetNumber(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserAuthentication")
-    @Results(id = "Authentication", value = {
-            @Result(property = "identityCode", column = "identity_code"),
-            @Result(property = "realname", column = "realname"),
-            @Result(property = "identityNumber", column = "identity_number"),
-            @Result(property = "schoolNumber", column = "school_number")
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Authentication.AuthenticationMapper.Authentication")
     public Authentication selectUserAuthentication(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserDeliveryOrderList")
-    @Results(id = "DeliveryOrder", value = {
-            @Result(property = "orderId", column = "order_id"),
-            @Result(property = "orderTime", column = "order_time", javaType = Date.class, jdbcType = JdbcType.TIMESTAMP),
-            @Result(property = "name", column = "name"),
-            @Result(property = "number", column = "number"),
-            @Result(property = "phone", column = "phone"),
-            @Result(property = "price", column = "price"),
-            @Result(property = "company", column = "company"),
-            @Result(property = "address", column = "address"),
-            @Result(property = "state", column = "state"),
-            @Result(property = "remarks", column = "remarks")
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Delivery.DeliveryMapper.DeliveryOrderDetail")
     public List<DeliveryOrder> selectUserDeliveryOrderList(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserDeliveryTradeList")
-    @Results(id = "DeliveryTrade", value = {
-            @Result(property = "tradeId", column = "trade_id"),
-            @Result(property = "orderId", column = "order_id"),
-            @Result(property = "createTime", column = "create_time", javaType = Date.class, jdbcType = JdbcType.TIMESTAMP),
-            @Result(property = "state", column = "state")
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Delivery.DeliveryMapper.DeliveryTrade")
     public List<DeliveryTrade> selectUserDeliveryTradeList(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserErshouItemList")
-    @Results(id = "Ershou", value = {
-            @Result(property = "id", column = "id"),
-            @Result(property = "name", column = "name"),
-            @Result(property = "description", column = "description"),
-            @Result(property = "price", column = "price"),
-            @Result(property = "location", column = "location"),
-            @Result(property = "type", column = "type"),
-            @Result(property = "qq", column = "qq"),
-            @Result(property = "phone", column = "phone"),
-            @Result(property = "state", column = "state"),
-            @Result(property = "publishTime", column = "publish_time", javaType = Date.class, jdbcType = JdbcType.TIMESTAMP)
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Ershou.ErshouMapper.ErshouItem")
     public List<ErshouItem> selectUserErshouItemList(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserLostAndFoundItemList")
-    @Results(id = "LostAndFound", value = {
-            @Result(property = "id", column = "id"),
-            @Result(property = "name", column = "name"),
-            @Result(property = "description", column = "description"),
-            @Result(property = "location", column = "location"),
-            @Result(property = "itemType", column = "item_type"),
-            @Result(property = "lostType", column = "lost_type"),
-            @Result(property = "state", column = "state"),
-            @Result(property = "publishTime", column = "publish_time", javaType = Date.class, jdbcType = JdbcType.TIMESTAMP)
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.LostAndFound.LostAndFoundMapper.LostAndFoundItem")
     public List<LostAndFoundItem> selectUserLostAndFoundItemList(String username);
 
     @SelectProvider(type = DataSQLBuilder.class, method = "selectUserSecretItemList")
-    @Results(id = "Secret", value = {
-            @Result(property = "id", column = "id"),
-            @Result(property = "theme", column = "theme"),
-            @Result(property = "content", column = "content"),
-            @Result(property = "type", column = "type"),
-            @Result(property = "timer", column = "timer"),
-            @Result(property = "state", column = "state"),
-            @Result(property = "publishTime", column = "publish_time"),
-            @Result(property = "likeCount", column = "like_count"),
-            @Result(property = "secretCommentList", column = "id", javaType = List.class
-                    , many = @Many(select = "edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Secret.SecretMapper.selectSecretComment"))
-    })
+    @ResultMap("edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.Secret.SecretMapper.SecretContent")
     public List<Secret> selectUserSecretItemList(String username);
 }

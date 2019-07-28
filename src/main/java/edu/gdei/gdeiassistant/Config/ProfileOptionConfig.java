5a02ef93e9172ecdf0ad8fd4064e786dd86edb43
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2019-07-29 05:48:31 +0800

    个人资料选项参数值写入常量表
    Former-commit-id: e078e421d1b2c606ae907ea6e48e92ce91ef40b4
    Former-commit-id: c4b56a3fb2cb5464332e964909cce0304f530ffb
    Former-commit-id: 5b0e2746cc325e1618b4451c108df56f1b4b17bc

--- src/main/java/edu/gdei/gdeiassistant/Config/ProfileOptionConfig.java
+++ src/main/java/edu/gdei/gdeiassistant/Config/ProfileOptionConfig.java
@@ -1,5 +1,6 @@
 package edu.gdei.gdeiassistant.Config;
 
+import edu.gdei.gdeiassistant.Constant.OptionConstantUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,10 +18,9 @@ public class ProfileOptionConfig {
     @Bean
     public Map<Integer, String> genderMap() {
         Map<Integer, String> genderMap = new HashMap<>();
-        genderMap.put(0, "未选择");
-        genderMap.put(1, "男");
-        genderMap.put(2, "女");
-        genderMap.put(3, "自定义");
+        for (int i = 0; i < OptionConstantUtils.GENDER_OPTIONS.length; i++) {
+            genderMap.put(i, OptionConstantUtils.GENDER_OPTIONS[i]);
+        }
         return genderMap;
     }
 
@@ -32,15 +32,9 @@ public class ProfileOptionConfig {
     @Bean
     public Map<Integer, String> degreeMap() {
         Map<Integer, String> degreeMap = new HashMap<>();
-        degreeMap.put(0, "未选择");
-        degreeMap.put(1, "小学");
-        degreeMap.put(2, "初中");
-        degreeMap.put(3, "中专/职高/技校");
-        degreeMap.put(4, "高中");
-        degreeMap.put(5, "专科");
-        degreeMap.put(6, "本科");
-        degreeMap.put(7, "硕士");
-        degreeMap.put(8, "博士");
+        for (int i = 0; i < OptionConstantUtils.DEGREE_OPTIONS.length; i++) {
+            degreeMap.put(i, OptionConstantUtils.DEGREE_OPTIONS[i]);
+        }
         return degreeMap;
     }
 
@@ -52,23 +46,9 @@ public class ProfileOptionConfig {
     @Bean
     public Map<Integer, String> facultyMap() {
         Map<Integer, String> facultyMap = new HashMap<>();
-        facultyMap.put(0, "未选择");
-        facultyMap.put(1, "教育学院");
-        facultyMap.put(2, "政法系");
-        facultyMap.put(3, "中文系");
-        facultyMap.put(4, "数学系");
-        facultyMap.put(5, "外语系");
-        facultyMap.put(6, "物理与信息工程系");
-        facultyMap.put(7, "化学系");
-        facultyMap.put(8, "生物与食品工程学院");
-        facultyMap.put(9, "体育学院");
-        facultyMap.put(10, "美术学院");
-        facultyMap.put(11, "计算机科学系");
-        facultyMap.put(12, "音乐系");
-        facultyMap.put(13, "教师研修学院");
-        facultyMap.put(14, "成人教育学院");
-        facultyMap.put(15, "网络教育学院");
-        facultyMap.put(16, "马克思主义学院");
+        for (int i = 0; i < OptionConstantUtils.FACULTY_OPTIONS.length; i++) {
+            facultyMap.put(i, OptionConstantUtils.FACULTY_OPTIONS[i]);
+        }
         return facultyMap;
     }
 }

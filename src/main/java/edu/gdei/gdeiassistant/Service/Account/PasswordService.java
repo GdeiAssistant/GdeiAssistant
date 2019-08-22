2a1025ba0aae91c16e0b7030591edc788774452f
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-08-22 21:00:54 +0800

    添加修改密码功能，支持非在校学生和教师用户在应用内修改账号密码

---
+++ src/main/java/edu/gdei/gdeiassistant/Service/Account/PasswordService.java
@@ -0,0 +1,38 @@
+package edu.gdei.gdeiassistant.Service.Account;
+
+import edu.gdei.gdeiassistant.Exception.CommonException.PasswordIncorrectException;
+import edu.gdei.gdeiassistant.Pojo.Entity.User;
+import edu.gdei.gdeiassistant.Repository.Mysql.GdeiAssistant.Mapper.User.UserMapper;
+import edu.gdei.gdeiassistant.Tools.StringEncryptUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 修改用户账号密码
+     *
+     * @param username
+     * @param oldPassword
+     * @param newPassword
+     * @throws Exception
+     */
+    public void ChangeUserPassword(String username, String oldPassword, String newPassword) throws Exception {
+        User user = userMapper.selectUser(StringEncryptUtils.encryptString(username));
+        if (user != null) {
+            User decryptedUser = user.decryptUser();
+            if (decryptedUser.getPassword().equals(oldPassword)) {
+                decryptedUser.setPassword(newPassword);
+                User encryptedUser = decryptedUser.encryptUser();
+                userMapper.updateUser(encryptedUser);
+            } else {
+                throw new PasswordIncorrectException("用户旧密码不匹配");
+            }
+        }
+    }
+
+}

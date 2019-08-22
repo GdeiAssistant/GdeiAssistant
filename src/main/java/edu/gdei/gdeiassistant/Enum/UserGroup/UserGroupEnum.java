6c7120f5da304300da91ad571d157142aed0e662
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-08-23 03:36:30 +0800

    添加用户组名称

--- src/main/java/edu/gdei/gdeiassistant/Enum/UserGroup/UserGroupEnum.java
+++ src/main/java/edu/gdei/gdeiassistant/Enum/UserGroup/UserGroupEnum.java
@@ -2,16 +2,25 @@ package edu.gdei.gdeiassistant.Enum.UserGroup;
 
 public enum UserGroupEnum {
 
-    ADMIN(1), STUDENT(2), TEST(3), TEACHER(4), SERVICE(5), GRADUATED(6), TRIAL(7);
+    ADMIN(1, "管理员"), STUDENT(2, "在校学生"), TEST(3, "测试用户"), TEACHER(4, "教师用户"),
+
+    SERVICE(5, "客服人员"), GRADUATED(6, "毕业学生"), TRIAL(7, "体验用户");
 
     private Integer value;
+
+    private String name;
 
     public Integer getValue() {
         return value;
     }
 
-    UserGroupEnum(Integer value) {
+    UserGroupEnum(Integer value, String name) {
         this.value = value;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public static UserGroupEnum getEnumByName(String name) {

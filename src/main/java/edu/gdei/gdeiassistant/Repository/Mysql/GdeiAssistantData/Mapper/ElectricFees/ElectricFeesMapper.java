e4da34bb47ae573d2d7faea7ba0bde6f9082a4ab
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-08-26 07:59:36 +0800

    修正SQL语句中表名大小写错误以及ORM映射中类属性和数据表列倒置的错误

--- src/main/java/edu/gdei/gdeiassistant/Repository/Mysql/GdeiAssistantData/Mapper/ElectricFees/ElectricFeesMapper.java
+++ src/main/java/edu/gdei/gdeiassistant/Repository/Mysql/GdeiAssistantData/Mapper/ElectricFees/ElectricFeesMapper.java
@@ -7,26 +7,26 @@ import java.util.List;
 
 public interface ElectricFeesMapper {
 
-    @Select("select * from electricFees where name=#{name} and number=#{number} and year=#{year}")
+    @Select("select * from electricfees where name=#{name} and number=#{number} and year=#{year}")
     @Results(id = "ElectricFees", value = {
-            @Result(property = "year", column = "year"),
-            @Result(property = "building_number", column = "buildingNumber"),
-            @Result(property = "room_number", column = "roomNumber"),
-            @Result(property = "people_number", column = "peopleNumber"),
-            @Result(property = "department", column = "department"),
-            @Result(property = "number", column = "number"),
-            @Result(property = "name", column = "name"),
-            @Result(property = "used_electric_amount", column = "usedElectricAmount"),
-            @Result(property = "free_electric_amount", column = "freeElectricAmount"),
-            @Result(property = "fee_based_electric_amount", column = "feeBasedElectricAmount"),
-            @Result(property = "electric_price", column = "electricPrice"),
-            @Result(property = "total_electric_bill", column = "totalElectricBill"),
-            @Result(property = "average_electric_bill", column = "averageElectricBill")
+            @Result(column = "year", property = "year"),
+            @Result(column = "building_number", property = "buildingNumber"),
+            @Result(column = "room_number", property = "roomNumber"),
+            @Result(column = "people_number", property = "peopleNumber"),
+            @Result(column = "department", property = "department"),
+            @Result(column = "number", property = "number"),
+            @Result(column = "name", property = "name"),
+            @Result(column = "used_electric_amount", property = "usedElectricAmount"),
+            @Result(column = "free_electric_amount", property = "freeElectricAmount"),
+            @Result(column = "fee_based_electric_amount", property = "feeBasedElectricAmount"),
+            @Result(column = "electric_price", property = "electricPrice"),
+            @Result(column = "total_electric_bill", property = "totalElectricBill"),
+            @Result(column = "average_electric_bill", property = "averageElectricBill")
     })
     public ElectricFees selectElectricFees(@Param("name") String name
             , @Param("number") Long number, @Param("year") Integer year);
 
-    @Insert("insert into electricFees (year,building_number,room_number,people_number" +
+    @Insert("insert into electricfees (year,building_number,room_number,people_number" +
             ",department,number,name,used_electric_amount,free_electric_amount" +
             ",fee_based_electric_amount,electric_price,total_electric_bill,average_electric_bill) values " +
             "<foreach collection='list' item='electricFees' index='index' separator=','> " +

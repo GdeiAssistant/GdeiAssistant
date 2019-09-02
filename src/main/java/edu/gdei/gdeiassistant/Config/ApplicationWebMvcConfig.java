d4f27ca072f2e9e33b49751f64820ceb106acd0e
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-09-02 15:45:06 +0800

    添加ViewController映射配置信息

--- src/main/java/edu/gdei/gdeiassistant/Config/ApplicationWebMvcConfig.java
+++ src/main/java/edu/gdei/gdeiassistant/Config/ApplicationWebMvcConfig.java
@@ -8,10 +8,7 @@ import edu.gdei.gdeiassistant.Interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
@@ -201,5 +198,10 @@ public class ApplicationWebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
         registry.addResourceHandler("/img/**").addResourceLocations("/img/");
         registry.addResourceHandler("/txt/MP_verify_i9vujYHtkV4q7Kgx.txt").addResourceLocations("/MP_verify_i9vujYHtkV4q7Kgx.txt");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
     }
 }

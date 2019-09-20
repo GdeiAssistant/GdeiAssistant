ccfa3415076a0d58c5c03951cac5c4b00a984f34
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2019-09-21 07:56:52 +0800

    使用异常统一处理器替代捕获异常处理，Redis的Key值使用SHA256进行哈希处理
    Former-commit-id: a40da49c67f039bf0194927d3847cf0e0555618a
    Former-commit-id: 65e9c9bf49562bfa49de6a10786e7978bb9e277e
    Former-commit-id: dd4e5a852f3db58412f55e955a84c65871d860cf

--- src/main/java/edu/gdei/gdeiassistant/Repository/Redis/UserCertificate/UserCertificateDaoImpl.java
+++ src/main/java/edu/gdei/gdeiassistant/Repository/Redis/UserCertificate/UserCertificateDaoImpl.java
@@ -2,6 +2,7 @@ package edu.gdei.gdeiassistant.Repository.Redis.UserCertificate;
 
 import edu.gdei.gdeiassistant.Pojo.Entity.User;
 import edu.gdei.gdeiassistant.Pojo.UserLogin.UserCertificate;
+import edu.gdei.gdeiassistant.Tools.StringEncryptUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -13,12 +14,15 @@ import java.util.concurrent.TimeUnit;
 @Repository
 public class UserCertificateDaoImpl implements UserCertificateDao {
 
+    private final String PREFIX = "USER_CERTIFICATE_";
+
     @Autowired
     private RedisTemplate<String, Map<String, String>> redisTemplate;
 
     @Override
     public UserCertificate queryUserCertificate(String username) {
-        Map<String, String> map = redisTemplate.opsForValue().get(username);
+        Map<String, String> map = redisTemplate.opsForValue().get(StringEncryptUtils
+                .SHA256HexString(PREFIX + username));
         if (map != null) {
             UserCertificate userCertificate = new UserCertificate();
             User user = new User();
@@ -41,7 +45,9 @@ public class UserCertificateDaoImpl implements UserCertificateDao {
         map.put("keycode", user.getKeycode());
         map.put("number", user.getNumber());
         map.put("timestamp", String.valueOf(userCertificate.getTimestamp()));
-        redisTemplate.opsForValue().set(user.getUsername(), map);
-        redisTemplate.expire(user.getUsername(), 1, TimeUnit.HOURS);
+        redisTemplate.opsForValue().set(StringEncryptUtils
+                .SHA256HexString(PREFIX + user.getUsername()), map);
+        redisTemplate.expire(StringEncryptUtils.SHA256HexString(PREFIX + user.getUsername())
+                , 1, TimeUnit.HOURS);
     }
 }

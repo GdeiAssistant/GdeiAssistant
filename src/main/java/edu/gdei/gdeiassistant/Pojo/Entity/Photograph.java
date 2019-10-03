74e0926776c78d15cf47313ddedf00ac1ff759bc
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-10-03 22:18:51 +0800

    添加拍好校园功能

---
+++ src/main/java/edu/gdei/gdeiassistant/Pojo/Entity/Photograph.java
@@ -0,0 +1,160 @@
+package edu.gdei.gdeiassistant.Pojo.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+
+@Component
+@Scope("prototype")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Photograph implements Serializable, Entity {
+
+    /**
+     * 照片信息ID
+     */
+    private Integer id;
+
+    /**
+     * 照片标题
+     */
+    @NotBlank
+    @Length(min = 1, max = 25)
+    private String title;
+
+    /**
+     * 照片数量
+     */
+    @NotNull
+    @Min(1)
+    @Max(4)
+    private Integer count;
+
+    /**
+     * 照片描述内容
+     */
+    @Length(max = 50)
+    private String content;
+
+    /**
+     * 照片类型，1为生活照，2为校园照
+     */
+    @NotNull
+    @Min(1)
+    @Max(2)
+    private Integer type;
+
+    /**
+     * 发布者用户名
+     */
+    private String username;
+
+    /**
+     * 发布时间
+     */
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+
+    /**
+     * 点赞数量
+     */
+    private Integer likeCount;
+
+    /**
+     * 评论数量
+     */
+    private Integer commentCount;
+
+    /**
+     * 是否已点赞
+     */
+    private Boolean liked;
+
+    public Integer getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(Integer likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public Integer getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(Integer commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Boolean getLiked() {
+        return liked;
+    }
+
+    public void setLiked(Boolean liked) {
+        this.liked = liked;
+    }
+}

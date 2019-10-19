3952eb81f484253f66479dcd465de9d7aa30b7c7
PeachShrubFizz <20649888+PeachShrubFizz@users.noreply.github.com>
2019-10-19 10:48:08 +0800

    TrialData注解添加RequestTime和ResponseTime属性，用于标记请求模拟数据和注入到返回值的时间信息属性名。将模拟数据的加载和后加工处理逻辑分离到服务层，修改模拟数据加载和后加工处理的逻辑

--- src/main/java/edu/gdei/gdeiassistant/Aspect/TrialDataAspect.java
+++ src/main/java/edu/gdei/gdeiassistant/Aspect/TrialDataAspect.java
@@ -1,14 +1,12 @@
 package edu.gdei.gdeiassistant.Aspect;
 
-import com.google.gson.Gson;
-import com.google.gson.internal.LinkedTreeMap;
-import com.google.gson.reflect.TypeToken;
 import edu.gdei.gdeiassistant.Annotation.TrialData;
 import edu.gdei.gdeiassistant.Enum.UserGroup.UserGroupEnum;
 import edu.gdei.gdeiassistant.Pojo.Entity.User;
 import edu.gdei.gdeiassistant.Pojo.Result.DataJsonResult;
 import edu.gdei.gdeiassistant.Pojo.Result.JsonResult;
 import edu.gdei.gdeiassistant.Service.Token.LoginTokenService;
+import edu.gdei.gdeiassistant.Service.TrailData.TrialDataService;
 import edu.gdei.gdeiassistant.Service.UserLogin.UserLoginService;
 import edu.gdei.gdeiassistant.Tools.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -18,7 +16,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
@@ -29,13 +26,13 @@ import javax.servlet.http.HttpServletRequest;
 public class TrialDataAspect {
 
     @Autowired
-    private Environment environment;
-
-    @Autowired
     private LoginTokenService loginTokenService;
 
     @Autowired
     private UserLoginService userLoginService;
+
+    @Autowired
+    private TrialDataService trialDataService;
 
     @Pointcut("@annotation(edu.gdei.gdeiassistant.Annotation.TrialData)")
     public void RequestAction() {
@@ -46,6 +43,7 @@ public class TrialDataAspect {
     public DataJsonResult CheckTrialUser(ProceedingJoinPoint proceedingJoinPoint, TrialData trialData) throws Throwable {
         Object[] args = proceedingJoinPoint.getArgs();
         HttpServletRequest request = (HttpServletRequest) args[0];
+        String username = (String) request.getSession().getAttribute("username");
         Integer group = (Integer) request.getSession().getAttribute("group");
         if (group == null) {
             //获取用户请求的权限令牌签名
@@ -53,39 +51,51 @@ public class TrialDataAspect {
             User user = userLoginService.GetUserByUsername(loginTokenService.ParseToken(token).get("username").asString());
             group = user.getGroup();
         }
-        //若当前用户组为体验用户，则返回测试数据
+        //若当前用户组为体验用户，则返回模拟结果数据
         if (UserGroupEnum.TRIAL.getValue().equals(group)) {
-            if (trialData.base()) {
-                return new Gson().fromJson(environment.getProperty("trial.data." + trialData.value())
-                        , new TypeToken<DataJsonResult>() {
-                        }.getType());
+            if (StringUtils.isBlank(username)) {
+                //解析权限令牌获取用户名
+                String token = (String) args[1];
+                username = loginTokenService.ParseToken(token).get("username").asString();
             }
-            DataJsonResult result = new Gson().fromJson(environment.getProperty("trial.data." + trialData.value())
-                    , new TypeToken<DataJsonResult>() {
-                    }.getType());
-            //若请求方法具有时间信息的属性，将会填充到测试数据中
-            if (StringUtils.isNotBlank(trialData.time())) {
-                Object value = null;
+            /*
+              若TrialData注解的请求时间信息RequestTime值不为空字符串，则表示请求的数据要求携带时间信息
+              将在请求参数中获取对应的属性值，该属性的名称与注解中RequestTime的值相同，将作为时间属性被用于获取模拟数据或模拟数据后加工的过程中
+              当该属性值为空时，系统将设置为默认时间属性值
+             */
+            Object requestTimeValue = null;
+            if (StringUtils.isNotBlank(trialData.requestTime())) {
                 //获取方法的所有参数值和名称，拦截具有时间信息的属性字段
                 String[] parameterNames = ((MethodSignature) proceedingJoinPoint.getSignature()).getParameterNames();
                 for (int i = 0; i < parameterNames.length; i++) {
-                    if (trialData.time().equals(parameterNames[i])) {
-                        value = args[i];
-                        //时间信息为空时，设置为默认值
-                        if (value == null) {
-                            Class clazz = ((MethodSignature) proceedingJoinPoint.getSignature())
-                                    .getMethod().getParameterTypes()[i];
-                            if (clazz.equals(Integer.class)) {
-                                value = 1;
-                            }
-                        }
+                    if (trialData.requestTime().equals(parameterNames[i])) {
+                        requestTimeValue = args[i];
+                        break;
                     }
                 }
-                //将时间属性填充入测试数据中与TrialData注解中time参数的名称值相同的属性中
-                ((LinkedTreeMap) result.getData()).put(trialData.time(), value);
             }
-            return result;
+            /*
+              若TrialData注解的响应时间信息ResponseTime值不为空字符串，则表示返回的模拟结果数据返回值中包含时间属性
+              将在请求参数中获取对应的属性值，该属性的名称与注解中ResponseTime的值相同，将作为时间属性被填充到测试数据中
+              当该属性值为空时，系统将设置为默认时间属性值
+             */
+            Object responseTimeValue = null;
+            if (StringUtils.isNotBlank(trialData.responseTime())) {
+                //获取方法的所有参数值和名称，拦截具有时间信息的属性字段
+                String[] parameterNames = ((MethodSignature) proceedingJoinPoint.getSignature()).getParameterNames();
+                for (int i = 0; i < parameterNames.length; i++) {
+                    if (trialData.responseTime().equals(parameterNames[i])) {
+                        responseTimeValue = args[i];
+                        break;
+                    }
+                }
+            }
+            //解析、加工并返回模拟结果数据
+            return trialDataService.ParseTrialData(trialData.value(), trialData.base()
+                    , trialData.requestTime(), requestTimeValue, trialData.responseTime()
+                    , responseTimeValue, username);
         }
+        //若非体验用户，则进行网络请求，返回真实数据
         if (trialData.base()) {
             return new DataJsonResult((JsonResult) proceedingJoinPoint.proceed(args));
         }

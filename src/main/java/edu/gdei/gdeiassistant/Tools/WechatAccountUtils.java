d6a8c14adf113eab0ace2524138be0d81ab7d195
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2019-11-10 06:06:55 +0800

    添加校园公众号数据和加载更新逻辑
    Former-commit-id: d80e31a768dad5866b795ad2d1f6153ad1b1f945
    Former-commit-id: e12e403bca73c76f3631539a07425097ab8ae3d6
    Former-commit-id: e35d3915144628f4e9d0869113247809073f647c

---
+++ src/main/java/edu/gdei/gdeiassistant/Tools/WechatAccountUtils.java
@@ -0,0 +1,53 @@
+package edu.gdei.gdeiassistant.Tools;
+
+import edu.gdei.gdeiassistant.Pojo.Entity.WechatAccount;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WechatAccountUtils {
+
+    private static List<WechatAccount> wechatAccountList = new ArrayList<>();
+
+    static {
+        //加载校园公众号列表
+        LoadSchoolWechatAccountData();
+    }
+
+    public static List<WechatAccount> getWechatAccountList() {
+        return wechatAccountList;
+    }
+
+    private static void LoadSchoolWechatAccountData() {
+        Resource resource = new ClassPathResource("/config/info/account.xml");
+        SAXReader saxReader = new SAXReader();
+        Document document = null;
+        try {
+            document = saxReader.read(resource.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Element root = document.getRootElement();
+        List<Element> accountElements = root.elements();
+        for (Element accountElement : accountElements) {
+            String id = accountElement.attributeValue("Id");
+            String biz = accountElement.attributeValue("Biz");
+            String name = accountElement.attributeValue("Name");
+            String description = accountElement.attributeValue("Description");
+            String avatar = accountElement.attributeValue("Avatar");
+            WechatAccount wechatAccount = new WechatAccount();
+            wechatAccount.setId(id);
+            wechatAccount.setBiz(biz);
+            wechatAccount.setName(name);
+            wechatAccount.setDescription(description);
+            wechatAccount.setAvatar(avatar);
+            WechatAccountUtils.wechatAccountList.add(wechatAccount);
+        }
+    }
+
+}

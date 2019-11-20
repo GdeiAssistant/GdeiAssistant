b693f39e5d4d6895a13054e405157c9408339823
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2019-11-20 19:43:24 +0800

    分离Controller和RestController，修改用户组权限注解属性，添加管理员通知公告发布页
    Former-commit-id: 7a49694fdd97a96e337f70bf7106440f5b555f9c
    Former-commit-id: 78f54b104549a3328b9b928f5946c5965e35cf62
    Former-commit-id: 1030c3479453971c23dd77b6c5c87dbae1d65d08

---
+++ src/main/webapp/js/announcement/publish.js
@@ -0,0 +1,41 @@
+//检测Input文字长度是否超过限制并进行实时字数反馈
+function inputLengthCheck(str, maxLen) {
+    if (str.value.length > maxLen) {
+        str.value = str.value.substr(0, maxLen);
+    }
+    $("#words").text(str.value.length);
+}
+
+//提交通知公告信息
+function publishAnnouncement() {
+    if ($("#title").val().length == 0 || $("#title").val().length > 50) {
+        $(".weui_warn").text("标题长度不合法").show().delay(2000).hide(0);
+    } else if ($("#content").val().length == 0 || $("#content").val().length > 250) {
+        $(".weui_warn").text("内容长度不合法").show().delay(2000).hide(0);
+    } else {
+        $(".weui-btn").attr("disabled", true);
+        let loading = weui.loading('提交中');
+        $.ajax({
+            url: '/api/announcement',
+            type: 'post',
+            data: {
+                title: $("#title").val(),
+                content: $("#content").val()
+            },
+            success: function (result) {
+                $(".weui-btn").attr("disabled", false);
+                loading.hide();
+                if (result.success === true) {
+                    $.toptip('提交成功', 'success');
+                } else {
+                    $.toptip(result.message, 'error');
+                }
+            },
+            error: function () {
+                $(".weui-btn").attr("disabled", false);
+                loading.hide();
+                $.toptip('网络异常，请检查网络连接', 'error');
+            }
+        });
+    }
+}

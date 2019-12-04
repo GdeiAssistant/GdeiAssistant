4ba03f0dd22d85295363f89029e71bb4ad31c88d
Pigbibi <20649888+pigbibi@users.noreply.github.com>
2019-12-04 12:51:50 +0800

    StateConstantUtils合并到ItemConstantUtils
    Former-commit-id: bd5a774ca0513d5a254b69da9f31327ad40b9500
    Former-commit-id: fc633f0cf95f83ce2453e5c8581e428c2559bfa0
    Former-commit-id: 79238ec8ee68d3a880e791befdb4f0ccef06c5c1

--- src/main/java/edu/gdei/gdeiassistant/Constant/ItemConstantUtils.java
+++ src/main/java/edu/gdei/gdeiassistant/Constant/ItemConstantUtils.java
@@ -15,4 +15,18 @@ public class ItemConstantUtils {
             , "租赁", "生活娱乐", "其他"};
 
     public static final String[] SECRET_ITEM_TYPE = {"文本", "语音"};
+
+    public static final String[] PHOTOGRAPH_ITEM_TYPE = {"生活照", "校园照"};
+
+    public static final String[] EXPRESS_ITEM_GENDER_TYPE = {"男", "女", "其他或保密"};
+
+    public static final String[] DELIVERY_ORDER_STATE_TYPE = {"未被接单", "已被接单", "用户自主删除", "系统删除"};
+
+    public static final String[] DELIVERY_TRADE_STATE_TYPE = {"等待完成", "已完成", "系统删除"};
+
+    public static final String[] ERSHOU_STATE_TYPE = {"下架", "待出售", "已出售", "系统删除"};
+
+    public static final String[] LOST_AND_FOUND_STATE_TYPE = {"寻主/寻物中", "确认寻回", "系统删除"};
+
+    public static final String[] SECRET_STATE_TYPE = {"已发布", "被定时删除", "系统删除"};
 }

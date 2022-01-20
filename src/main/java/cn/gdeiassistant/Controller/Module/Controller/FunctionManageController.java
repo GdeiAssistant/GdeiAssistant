1e7d16026eecd3b473f5ab2e91541d44ef1a8a6d
Pigbibi <20649888+Pigbibi@users.noreply.github.com>
2022-01-20 16:34:50 +0800

    更改功能模块路径名

--- src/main/java/cn/gdeiassistant/Controller/Module/Controller/FunctionManageController.java
+++ src/main/java/cn/gdeiassistant/Controller/Module/Controller/FunctionManageController.java
@@ -1,4 +1,4 @@
-package cn.gdeiassistant.Controller.FunctionManage.Controller;
+package cn.gdeiassistant.Controller.Module.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

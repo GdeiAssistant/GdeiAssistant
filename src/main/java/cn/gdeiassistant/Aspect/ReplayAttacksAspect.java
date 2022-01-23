826f758fe80abde1000f47782de6feb34f08fa4e
Pigbibi <20649888+Pigbibi@users.noreply.github.com>
2022-01-23 18:26:04 +0800

    添加功能模块检测功能，开发模式下将提示未启用的功能模块信息

--- src/main/java/cn/gdeiassistant/Aspect/ReplayAttacksAspect.java
+++ src/main/java/cn/gdeiassistant/Aspect/ReplayAttacksAspect.java
@@ -3,18 +3,18 @@ package cn.gdeiassistant.Aspect;
 import cn.gdeiassistant.Exception.RequestValidException.NonceInvalidException;
 import cn.gdeiassistant.Exception.RequestValidException.SignInvalidException;
 import cn.gdeiassistant.Exception.RequestValidException.TimestampInvalidException;
+import cn.gdeiassistant.Pojo.Config.ReplayAttacksValidateConfig;
 import cn.gdeiassistant.Pojo.Entity.RequestValidation;
 import cn.gdeiassistant.Repository.Redis.Request.RequestDao;
-import cn.gdeiassistant.Tools.SpringProfileUtils;
-import cn.gdeiassistant.Tools.StringEncryptUtils;
-import cn.gdeiassistant.Tools.StringUtils;
+import cn.gdeiassistant.Tools.Utils.SpringProfileUtils;
+import cn.gdeiassistant.Tools.Utils.StringEncryptUtils;
+import cn.gdeiassistant.Tools.Utils.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -29,12 +29,8 @@ public class ReplayAttacksAspect {
     @Autowired
     private RequestDao requestDao;
 
-    private String token;
-
-    @Value("#{propertiesReader['request.validate.token']}")
-    public void setToken(String token) {
-        this.token = token;
-    }
+    @Autowired
+    private ReplayAttacksValidateConfig replayAttacksValidateConfig;
 
     @Pointcut("@annotation(cn.gdeiassistant.Annotation.ReplayAttacksProtection)")
     public void LogicAction() {
@@ -45,7 +41,7 @@ public class ReplayAttacksAspect {
     public void ValidateRequest(JoinPoint joinPoint) throws TimestampInvalidException, SignInvalidException, NonceInvalidException {
         Object[] args = joinPoint.getArgs();
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-        //检测是否为开发环境
+        //检测是否开启防重放攻击验证模块
         if (!SpringProfileUtils.CheckDevelopmentEnvironment()) {
             //获取请求的随机值、时间戳和签名
             RequestValidation requestValidation = null;
@@ -59,7 +55,7 @@ public class ReplayAttacksAspect {
                     && requestValidation.getTimestamp() != null
                     && StringUtils.isNotBlank(requestValidation.getSignature())
                     && requestValidation.getSignature().equals(StringEncryptUtils.SHA1HexString(requestValidation.getTimestamp()
-                    + requestValidation.getNonce() + token))) {
+                    + requestValidation.getNonce() + replayAttacksValidateConfig.getToken()))) {
                 //校验时间戳，请求时间戳与当前时间戳不超过60s
                 if (requestValidation.getTimestamp() != null && Duration.between(Instant.ofEpochMilli(requestValidation.getTimestamp()), Instant.now()).getSeconds() >= -5
                         && Duration.between(Instant.now(), Instant.ofEpochMilli(requestValidation.getTimestamp())).getSeconds() <= 60) {

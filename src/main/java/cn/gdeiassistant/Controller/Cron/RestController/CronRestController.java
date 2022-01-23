72cc134a6b9ec835d2ee1107ed95ac979d920ae7
Pigbibi <20649888+Pigbibi@users.noreply.github.com>
2022-01-23 14:44:15 +0800

    设置CronService为Nullable

--- src/main/java/cn/gdeiassistant/Controller/Cron/RestController/CronRestController.java
+++ src/main/java/cn/gdeiassistant/Controller/Cron/RestController/CronRestController.java
@@ -10,53 +10,67 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 @RestController
 public class CronRestController {
 
-    @Autowired
+    @Autowired(required = false)
     private GradeCronService gradeCronService;
 
-    @Autowired
+    @Autowired(required = false)
     private ScheduleCronService scheduleCronService;
 
-    @Autowired
+    @Autowired(required = false)
     private WechatCronService wechatCronService;
 
-    @Autowired
+    @Autowired(required = false)
     private SchoolNewsCornService schoolNewsCornService;
 
     @RequestMapping(value = "/cron/grade", method = RequestMethod.GET)
-    public JsonResult CacheGradeData(HttpServletRequest request) {
-        gradeCronService.SynchronizeGradeData();
-        return new JsonResult(true);
+    public JsonResult CacheGradeData() {
+        if (gradeCronService != null) {
+            gradeCronService.SynchronizeGradeData();
+            return new JsonResult(true);
+        }
+        return new JsonResult(false);
     }
 
     @RequestMapping(value = "/cron/schedule", method = RequestMethod.GET)
-    public JsonResult CacheScheduleData(HttpServletRequest request) {
-        scheduleCronService.SynchronizeScheduleData();
-        return new JsonResult(true);
+    public JsonResult CacheScheduleData() {
+        if (scheduleCronService != null) {
+            scheduleCronService.SynchronizeScheduleData();
+            return new JsonResult(true);
+        }
+        return new JsonResult(false);
     }
 
     @RequestMapping(value = "/cron/wechat/reading", method = RequestMethod.GET)
-    public JsonResult SyncWechatReadingData(HttpServletRequest request) {
-        wechatCronService.SyncWechatReadingItem();
-        return new JsonResult(true);
+    public JsonResult SyncWechatReadingData() {
+        if (wechatCronService != null) {
+            wechatCronService.SyncWechatReadingItem();
+            return new JsonResult(true);
+        }
+        return new JsonResult(false);
     }
 
     @RequestMapping(value = "/cron/wechat/account", method = RequestMethod.GET)
-    public JsonResult UpdateWechatAccountData(HttpServletRequest request) {
-        wechatCronService.UpdateAccountData();
-        return new JsonResult(true);
+    public JsonResult UpdateWechatAccountData() {
+        if (wechatCronService != null) {
+            wechatCronService.UpdateAccountData();
+            return new JsonResult(true);
+        }
+        return new JsonResult(false);
     }
 
     @RequestMapping(value = "/cron/news", method = RequestMethod.GET)
-    public JsonResult CollectNews(HttpServletRequest request) throws InterruptedException, ExecutionException, IOException {
-        schoolNewsCornService.CollectNews();
-        return new JsonResult(true);
+    public JsonResult CollectNews() throws InterruptedException, ExecutionException, IOException {
+        if (schoolNewsCornService != null) {
+            schoolNewsCornService.CollectNews();
+            return new JsonResult(true);
+        }
+        return new JsonResult(false);
     }
 
 }

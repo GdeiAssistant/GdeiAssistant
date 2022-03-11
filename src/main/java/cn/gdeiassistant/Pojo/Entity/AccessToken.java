413c2800562899032005b19035ac1fdbb74f2bba
Pigbibi <20649888+Pigbibi@users.noreply.github.com>
2022-03-12 07:30:49 +0800

    删除SimpleToken类

--- src/main/java/cn/gdeiassistant/Pojo/Entity/AccessToken.java
+++ src/main/java/cn/gdeiassistant/Pojo/Entity/AccessToken.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 /**
- * 用户使用移动端登录后，服务端会在系统中生成一个令牌信息，包含用户名、设备MAC地址、请求的IP地址、时间戳等信息
+ * 用户使用移动端登录后，服务端会在系统中生成一个令牌信息，包含用户名、时间戳等信息
  * 系统生成权限令牌的同时，还将对令牌进行签名，将签名信息返回给移动端
  * 权限令牌有效期为7天
  */
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class AccessToken extends SimpleToken {
+public class AccessToken extends Token {
 
     /**
      * 用户名

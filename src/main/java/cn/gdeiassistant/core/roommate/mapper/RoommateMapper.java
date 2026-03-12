c4dc18a2efa45674a6b97041f85ee36025693cd9
Pigbibi <20649888+Pigbibi@users.noreply.github.com>
2026-03-12 16:13:23 +0800

    Finalize unified interaction model and remove legacy dating message flow

--- src/main/java/cn/gdeiassistant/core/roommate/mapper/RoommateMapper.java
+++ src/main/java/cn/gdeiassistant/core/roommate/mapper/RoommateMapper.java
@@ -1,9 +1,15 @@
 package cn.gdeiassistant.core.roommate.mapper;
 
-import cn.gdeiassistant.core.roommate.pojo.entity.RoommateMessageEntity;
 import cn.gdeiassistant.core.roommate.pojo.entity.RoommatePickEntity;
 import cn.gdeiassistant.core.roommate.pojo.entity.RoommateProfileEntity;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -110,75 +116,11 @@ public interface RoommateMapper {
     })
     List<RoommatePickEntity> selectRoommatePickListByUsername(@Param("username") String username);
 
-    @Select("select message_id as datingMessageMessageId,dating_message.username as datingMessageUsername,type as datingMessageType,dating_message.state as datingMessageState,dating_message.create_time as datingMessageCreateTime," +
-            "dating_pick.pick_id as datingPickPickId,dating_pick.username as datingPickUsername,dating_pick.content as datingPickContent,dating_pick.state as datingPickState," +
-            "dating_profile.username as datingProfileUsername,dating_profile.nickname as datingProfileNickname,dating_profile.profile_id as datingProfileProfileId " +
-            "from dating_message,dating_pick,dating_profile " +
-            "where dating_message.pick_id=dating_pick.pick_id and dating_pick.profile_id=dating_profile.profile_id " +
-            "and dating_message.username=#{username} order by dating_message.create_time desc, dating_message.message_id desc limit #{start},#{size}")
-    @Results(id = "RoommateMessage", value = {
-            @Result(property = "messageId", column = "datingMessageMessageId"),
-            @Result(property = "username", column = "datingMessageUsername"),
-            @Result(property = "type", column = "datingMessageType"),
-            @Result(property = "state", column = "datingMessageState"),
-            @Result(property = "createTime", column = "datingMessageCreateTime"),
-            @Result(property = "roommatePick.pickId", column = "datingPickPickId"),
-            @Result(property = "roommatePick.username", column = "datingPickUsername"),
-            @Result(property = "roommatePick.content", column = "datingPickContent"),
-            @Result(property = "roommatePick.state", column = "datingPickState"),
-            @Result(property = "roommatePick.roommateProfile.profileId", column = "datingProfileProfileId"),
-            @Result(property = "roommatePick.roommateProfile.username", column = "datingProfileUsername"),
-            @Result(property = "roommatePick.roommateProfile.nickname", column = "datingProfileNickname"),
-            @Result(property = "roommatePick.roommateProfile.grade", column = "grade"),
-            @Result(property = "roommatePick.roommateProfile.faculty", column = "faculty"),
-            @Result(property = "roommatePick.roommateProfile.hometown", column = "hometown"),
-            @Result(property = "roommatePick.roommateProfile.content", column = "content"),
-            @Result(property = "roommatePick.roommateProfile.qq", column = "qq"),
-            @Result(property = "roommatePick.roommateProfile.wechat", column = "wechat"),
-            @Result(property = "roommatePick.roommateProfile.area", column = "area"),
-            @Result(property = "roommatePick.roommateProfile.state", column = "state")
-    })
-    List<RoommateMessageEntity> selectUserRoommateMessagePage(@Param("username") String username
-            , @Param("start") Integer start, @Param("size") Integer size);
-
-    /**
-     * interaction 聚合接口专用分页查询。
-     */
-    @Select("select message_id as datingMessageMessageId,dating_message.username as datingMessageUsername,type as datingMessageType,dating_message.state as datingMessageState,dating_message.create_time as datingMessageCreateTime," +
-            "dating_pick.pick_id as datingPickPickId,dating_pick.username as datingPickUsername,dating_pick.content as datingPickContent,dating_pick.state as datingPickState," +
-            "dating_profile.username as datingProfileUsername,dating_profile.nickname as datingProfileNickname,dating_profile.profile_id as datingProfileProfileId " +
-            "from dating_message,dating_pick,dating_profile " +
-            "where dating_message.pick_id=dating_pick.pick_id and dating_pick.profile_id=dating_profile.profile_id " +
-            "and dating_message.username=#{username} order by dating_message.create_time desc, dating_message.message_id desc limit #{start},#{size}")
-    @ResultMap("RoommateMessage")
-    List<RoommateMessageEntity> selectUserRoommateMessageInteractionPage(@Param("username") String username,
-            @Param("start") Integer start, @Param("size") Integer size);
-
-    @Select("select message_id as datingMessageMessageId,dating_message.username as datingMessageUsername,type as datingMessageType,dating_message.state as datingMessageState,dating_message.create_time as datingMessageCreateTime," +
-            "dating_pick.pick_id as datingPickPickId,dating_pick.username as datingPickUsername,dating_pick.content as datingPickContent,dating_pick.state as datingPickState," +
-            "dating_profile.username as datingProfileUsername,dating_profile.nickname as datingProfileNickname,dating_profile.profile_id as datingProfileProfileId " +
-            "from dating_message,dating_pick,dating_profile " +
-            "where dating_message.pick_id=dating_pick.pick_id and dating_pick.profile_id=dating_profile.profile_id " +
-            "and dating_message.username=#{username} and dating_message.state=0 " +
-            "order by dating_message.create_time desc, dating_message.message_id desc")
-    @ResultMap("RoommateMessage")
-    List<RoommateMessageEntity> selectUnreadUserRoommateMessageInteractionList(@Param("username") String username);
-
-    @Select("select count(message_id) from dating_message where username=#{username} and state=0")
-    @ResultType(Integer.class)
-    Integer selectUserUnReadRoommateMessageCount(String username);
-
-    @Insert("insert into dating_message (username,pick_id,type,state,create_time) values(#{username},#{roommatePick.pickId},#{type},#{state},now())")
-    void insertRoommateMessage(RoommateMessageEntity entity);
-
-    @Update("update dating_message set state=#{state} where message_id=#{messageId}")
-    void updateRoommateMessageState(@Param("messageId") Integer messageId, @Param("state") Integer state);
-
-    @Update("update dating_message set state=#{state} where message_id=#{messageId} and username=#{username}")
-    int updateRoommateMessageStateByUsername(@Param("messageId") Integer messageId,
-            @Param("state") Integer state,
-            @Param("username") String username);
-
-    @Update("update dating_message set state=#{state} where username=#{username} and state<>#{state}")
-    int updateAllRoommateMessageStateByUsername(@Param("username") String username, @Param("state") Integer state);
+    @Select("select p.pick_id as pick_id,p.username as pick_username,p.content as pick_content,p.state as pick_state," +
+            "d.profile_id as profile_id,d.username as profile_username,d.nickname,d.grade,d.faculty,d.hometown," +
+            "d.content as profile_content,d.qq,d.wechat,d.area,d.state as profile_state " +
+            "from dating_pick p inner join dating_profile d on p.profile_id=d.profile_id " +
+            "where d.username=#{username} order by p.pick_id desc")
+    @ResultMap("RoommatePickSent")
+    List<RoommatePickEntity> selectReceivedRoommatePickListByProfileOwner(@Param("username") String username);
 }

898691f54b4eb26c1587f78c6dcbfc3ab958fd23
Pigbibi <20649888+Pigbibi@users.noreply.github.com>
2026-03-26 06:04:23 +0800

    test: update profile contract expectations

--- src/test/java/cn/gdeiassistant/contract/ProfileOptionsContractTest.java
+++ src/test/java/cn/gdeiassistant/contract/ProfileOptionsContractTest.java
@@ -1,7 +1,6 @@
 package cn.gdeiassistant.contract;
 
 import cn.gdeiassistant.core.userProfile.controller.ProfileController;
-import cn.gdeiassistant.core.userProfile.service.ProfileLocalizationService;
 import cn.gdeiassistant.core.userProfile.service.ProfileOptionsFacade;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,22 +20,20 @@ class ProfileOptionsContractTest {
     void setUp() {
         ProfileController controller = new ProfileController();
         ProfileOptionsFacade facade = new ProfileOptionsFacade();
-        ProfileLocalizationService localizationService = new ProfileLocalizationService();
-        ReflectionTestUtils.setField(facade, "profileLocalizationService", localizationService);
         ReflectionTestUtils.setField(controller, "profileOptionsFacade", facade);
-        ReflectionTestUtils.setField(controller, "profileLocalizationService", localizationService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
-    void getProfileOptionsReturnsStructuredMajorPayload() throws Exception {
+    void getProfileOptionsReturnsCodeOnlyDictionaryPayload() throws Exception {
         mockMvc.perform(get("/api/profile/options").header("Accept-Language", "en-US"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data.faculties[0].code").value(0))
-                .andExpect(jsonPath("$.data.faculties[0].majors[0].code").value("unselected"))
-                .andExpect(jsonPath("$.data.faculties[11].majors[1].code").value("software_engineering"))
-                .andExpect(jsonPath("$.data.faculties[11].majors[1].label").isNotEmpty())
-                .andExpect(jsonPath("$.data.marketplaceItemTypes[0].label").isNotEmpty());
+                .andExpect(jsonPath("$.data.faculties[0].majors[0]").value("unselected"))
+                .andExpect(jsonPath("$.data.faculties[11].majors[1]").value("software_engineering"))
+                .andExpect(jsonPath("$.data.marketplaceItemTypes[0]").isNumber())
+                .andExpect(jsonPath("$.data.lostFoundItemTypes[0]").isNumber())
+                .andExpect(jsonPath("$.data.lostFoundModes[0]").isNumber());
     }
 }
